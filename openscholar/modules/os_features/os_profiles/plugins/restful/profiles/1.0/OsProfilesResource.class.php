021232bf6b15a13037896bbd5caef5cd66eed6b1
Amitaibu <amitai@gizra.com>
2014-05-07 13:08:15 +0300

    Add properties, and fix bundle.

--- openscholar/modules/os_features/os_profiles/plugins/restful/profiles/1.0/OsProfilesResource.class.php
+++ openscholar/modules/os_features/os_profiles/plugins/restful/profiles/1.0/OsProfilesResource.class.php
@@ -12,7 +12,40 @@ class OsProfilesResource extends RestfulEntityBaseNode {
    */
   public function getPublicFields() {
     $public_fields = parent::getPublicFields();
-    
+
+    $public_fields['description'] = array(
+      'property' => 'body',
+      'sub_property' => 'value',
+    );
+
+    $public_fields['address'] = array(
+      'property' => 'field_address',
+    );
+
+    $public_fields['email'] = array(
+      'property' => 'field_email',
+    );
+
+    $public_fields['first_name'] = array(
+      'property' => 'field_first_name',
+    );
+
+    $public_fields['last_name'] = array(
+      'property' => 'field_last_name',
+    );
+
+    $public_fields['phone'] = array(
+      'property' => 'field_phone',
+    );
+
+    $public_fields['professional_title'] = array(
+      'property' => 'field_professional_title',
+    );
+
+    $public_fields['website'] = array(
+      'property' => 'field_website',
+    );
+
     $public_fields['photo'] = array(
       'property' => 'field_person_photo',
       'process_callback' => array($this, 'processPhoto'),
