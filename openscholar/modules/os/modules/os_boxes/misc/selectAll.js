ee2b213f482742c82beb80e476b820e5d801fc88
ran81 <ran@gizra.com>
2014-06-05 10:30:43 +0800

    Make the "select all" unchecked when unchecking other checkboxes.

--- openscholar/modules/os/modules/os_boxes/misc/selectAll.js
+++ openscholar/modules/os/modules/os_boxes/misc/selectAll.js
@@ -2,18 +2,37 @@
  * 
  */
 
-Drupal.behaviors.selectAllStart = {
+(function ($) {
+  Drupal.behaviors.selectAllStart = {
     attach: function (ctx) {
-    	function selectAll() {
-    		var stop = this.name.lastIndexOf('['),
-    			name = this.name.slice(0,stop),
-    			chk = this.checked,
-    			$cbxs = jQuery('input[name^="'+name+'"]');
-    		$cbxs.each(function() {
-    			this.checked = chk;
-    		});
-    	}
-    	
-    	jQuery('input[name$="[all]"]').change(selectAll);
+
+      var select_all_cbx = $('input[name$="[all]"]');
+      var select_all_cbx_html = select_all_cbx.get(0);
+
+      var stop = select_all_cbx_html.name.lastIndexOf('['),
+        name = select_all_cbx_html.name.slice(0,stop),
+        $cbxs = $('input[name^="'+name+'"]');
+
+      function selectAll() {
+        var chk = select_all_cbx_html.checked;
+
+          $cbxs.each(function() {
+          this.checked = chk;
+        });
+      }
+
+      // Check/uncheck all checkboxes when the "Select All" is toggled.
+      select_all_cbx.change(selectAll);
+
+      // When unchecking an option that is not the "all" option, uncheck the
+      // "all" checkbox.
+      $cbxs.on('change', function(e) {
+        console.log(e.target);
+        if (!e.target.checked && e.target.value != 'all') {
+          select_all_cbx.attr('checked', false);
+        }
+
+      });
     }
-};
+  };
+})(jQuery);
