1f4f17a790ee664bc1b12b141b42113471cb3ebb
Roy Segall <roy.segall@gizra.com>
2014-06-08 14:49:04 +0300

    Remove un needed code.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/feed/OsImporterFeedValidator.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/feed/OsImporterFeedValidator.php
@@ -5,22 +5,6 @@
 class OsImporterFeedValidator extends NodeValidate {
 
   public function getFieldsInfo() {
-    return parent::getFieldsInfo() +  array(
-      'body' => array(
-        'preprocess' => array(
-          array($this, 'preprocessText'),
-        ),
-      ),
-      'field_semester' => array(
-        'validators' => array(
-          array($this, 'validationSemester'),
-        ),
-      ),
-      'field_offered_year' => array(
-        'preprocess' => array(
-          array($this, 'preprocessOfferedYear'),
-        ),
-      ),
-    );
+    return parent::getFieldsInfo();
   }
 }
