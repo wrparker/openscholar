61c09d40aaec9e4d908f57b7e5655c495e606d5b
Roy Segall <roy.segall@gizra.com>
2014-06-09 13:14:56 +0300

    Validating the feed CSV.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/feed/OsImporterFeedValidator.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/feed/OsImporterFeedValidator.php
@@ -1,10 +1,29 @@
 <?php
-/**
- * Required: title, url.
- */
+
 class OsImporterFeedValidator extends NodeValidate {
 
   public function getFieldsInfo() {
-    return parent::getFieldsInfo();
+    return parent::getFieldsInfo() + array(
+      'field_url' => array(
+        'validators' => array(
+          array($this, 'validatorUrlNotEmpty')
+        ),
+      ),
+    );
+  }
+
+  /**
+   * Validating the url field is not empty. We can't use the isNotEmpty since
+   * the field is array with keys.
+   *
+   * @param $value
+   *  The field value.
+   * @param $field
+   *  The field name.
+   */
+  public function validatorUrlNotEmpty($value, $field) {
+    if (empty($value['url'])) {
+      $this->setError(t('The url of the feed is empty. Please supply a feeds with URL.'));
+    }
   }
 }
