8e990c2e6144c91d83e96894e57682baee02dbe0
Roy Segall <roy.segall@gizra.com>
2014-06-09 13:22:45 +0300

    Validate link importers.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/link/OsImporterLinkValidator.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/link/OsImporterLinkValidator.php
@@ -1,10 +1,28 @@
 <?php
-/**
- * Required: title, url.
- */
 class OsImporterLinkValidator extends NodeValidate {
 
   public function getFieldsInfo() {
-    return parent::getFieldsInfo();
+    return parent::getFieldsInfo() + array(
+      'field_links_link' => array(
+        'validator' => array(
+          array($this, 'validatorUrlNotEmpty'),
+        ),
+      ),
+    );
+  }
+
+  /**
+   * Validating the url field is not empty. We can't use the isNotEmpty since
+   * the field is array with keys.
+   *
+   * @param $value
+   *  The field value.
+   * @param $field
+   *  The field name.
+   */
+  public function validatorUrlNotEmpty($value, $field) {
+    if (empty($value['url'])) {
+      $this->setError(t('You must supply a link with URL.'));
+    }
   }
 }
