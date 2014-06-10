a1195d7ee2bd53b8e3895f8cef29fb3949dee3d9
Roy Segall <roy.segall@gizra.com>
2014-06-10 11:20:59 +0300

    Rename the files plugins.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/presentation/OsImporterPresentationValidator.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/presentation/OsImporterPresentationValidator.php
@@ -6,4 +6,13 @@
  */
 class OsImporterPresentationValidator extends NodeValidate {
 
+  public function getFieldsInfo() {
+    return parent::getFieldsInfo() + array(
+      'field_links_link' => array(
+        'validator' => array(
+          array($this, 'validatorUrlNotEmpty'),
+        ),
+      ),
+    );
+  }
 }
