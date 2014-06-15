9a1333ce497141011eadc004e4dc98ca750d4989
Roy Segall <roy.segall@gizra.com>
2014-06-15 17:22:13 +0300

    Finish with the person validator.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/person/OsImporterPersonValidator.class.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/person/OsImporterPersonValidator.class.php
@@ -1,9 +1,61 @@
 <?php
 
 /**
- * required first name, last name
- * validation photo (minimum 220x220)
+ * Contains OsImporterPersonValidator
  */
 class OsImporterPersonValidator extends OsImporterEntityValidateBase {
 
+  public function setFieldsInfo() {
+    $fields = parent::setFieldsInfo();
+
+    $fields['field_first_name'] = array(
+      'validators' => array(
+        'isNotEmpty',
+      ),
+    );
+
+    $fields['field_last_name'] = array(
+      'validators' => array(
+        'isNotEmpty',
+      ),
+    );
+
+    $fields['person_photo'] = array(
+      'validators' => array(
+        'validatorPersonPhoto',
+      ),
+    );
+
+    return $fields;
+  }
+
+  /**
+   * Validating the image is in 220X220.
+   */
+  public function validatorPersonPhoto($field_name, $value) {
+    list($width, $height) = getimagesize(reset($value));
+
+    if ($width != 220 && $height != 220) {
+      $params = array(
+        '@width' => $width,
+        '@height' => $height,
+      );
+
+      $this->setError($field_name, 'The size of the image need to be 220X220. The given image is @widthX@height', $params);
+    }
+  }
+
+  /**
+   * Overriding parent:isNotEmpty().
+   *
+   * The person node don't need title by default since the title is generated
+   * from the first\last\middle name and we already verifying the titles.
+   */
+  public function isNotEmpty($field_name, $value) {
+    if ($field_name == 'title') {
+      return;
+    }
+
+    parent::isNotEmpty($field_name, $value);
+  }
 }
