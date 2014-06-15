345192e15c8ec1668edda88b7271648249faca09
Roy Segall <roy.segall@gizra.com>
2014-06-15 16:15:09 +0300

    Adding validation for feed.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/feed/OsImporterFeedValidator.class.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/feed/OsImporterFeedValidator.class.php
@@ -5,7 +5,7 @@ class OsImporterFeedValidator extends OsImporterEntityValidateBase {
   public function setFieldsInfo() {
     $fields = parent::setFieldsInfo();
 
-    $fields['field_url'] = array(
+    $fields['field_url__url'] = array(
       'validators' => array(
         'validatorUrlNotEmpty',
       ),
