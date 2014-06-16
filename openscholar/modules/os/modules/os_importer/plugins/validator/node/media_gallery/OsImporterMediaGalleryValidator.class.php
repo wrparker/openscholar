6cdce52a0026dd4f0a2393659a7f000b8fd7792f
Roy Segall <roy.segall@gizra.com>
2014-06-16 12:09:18 +0300

    Working on the gallery validation.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/media_gallery/OsImporterMediaGalleryValidator.class.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/media_gallery/OsImporterMediaGalleryValidator.class.php
@@ -6,4 +6,22 @@
  */
 class OsImporterMediaGalleryValidator extends OsImporterEntityValidateBase {
 
+  public function setFieldsInfo() {
+    $fields = parent::setFieldsInfo();
+
+    $fields['media_gallery_rows'] = $fields['media_gallery_columns'] = array(
+      'validators' => array(
+        'validateRowsColumns'
+      ),
+    );
+
+    return $fields;
+  }
+
+  public function validateRowsColumns($field_name, $value) {
+    if (empty($value) || $value < 1) {
+      $params['@value'] = $value;
+      $this->setError($field_name, 'The field @field positive. The given value is: @value.', $params);
+    }
+  }
 }
