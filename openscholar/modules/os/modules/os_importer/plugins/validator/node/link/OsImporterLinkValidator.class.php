60c3730ba952cfbb5825b23c7571b58e7be34325
Shushu Inbar <shushu@gizra.com>
2014-06-16 09:35:17 +0300

    Fix validators and entity callbacks for links import.

--- openscholar/modules/os/modules/os_importer/plugins/validator/node/link/OsImporterLinkValidator.class.php
+++ openscholar/modules/os/modules/os_importer/plugins/validator/node/link/OsImporterLinkValidator.class.php
@@ -4,8 +4,8 @@ class OsImporterLinkValidator extends OsImporterEntityValidateBase {
   public function setFieldsInfo() {
     $fields = parent::setFieldsInfo();
 
-    $fields['field_links_link'] = array(
-      'validator' => array(
+    $fields['field_links_link__url'] = array(
+      'validators' => array(
         'validatorUrlNotEmpty',
       ),
     );
