491d8023b98458022c5377ad63e42a2f7d18eafd
ran81 <ran@gizra.com>
2014-07-06 21:58:02 +0800

    Work on activity stream.

--- openscholar/modules/os/modules/os_activity/plugins/restful/activities/1.0/OsActivitiesResource.class.php
+++ openscholar/modules/os/modules/os_activity/plugins/restful/activities/1.0/OsActivitiesResource.class.php
@@ -6,4 +6,15 @@
  */
 
 class OsActivitiesResource extends RestfulEntityBaseMultipleBundles {
+
+  /**
+   * Define the bundles to exposed to the API.
+   *
+   * @var array
+   *  Array keyed by bundle machine, and the RESTful resource as the value.
+   */
+  protected $bundles = array(
+    'os_create_node' => 'activities',
+    'os_edit_node' => 'activities',
+  );
 }
