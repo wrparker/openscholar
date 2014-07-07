6add856cce033710730b7aba3b36eea705be9135
ran81 <ran@gizra.com>
2014-07-07 19:42:09 +0800

    Fix creation of a profile from site.

--- openscholar/modules/os_features/os_biocv/plugins/restful/bios/1.0/OsBiosResource.class.php
+++ openscholar/modules/os_features/os_biocv/plugins/restful/bios/1.0/OsBiosResource.class.php
@@ -18,6 +18,20 @@ class OsBiosResource extends RestfulEntityBaseNode {
       'sub_property' => 'value',
     );
 
+    $public_fields['site_name'] = array(
+      'property' => 'og_group_ref',
+      'sub_property' => 'title',
+      'process_callback' => array($this, 'processGroup'),
+    );
+
     return $public_fields;
+  }
+
+  /**
+   * Process callback; Returns the title of the VSite this bio belongs to.
+   */
+  protected function processGroup($value) {
+    $site_name = $value[0];
+    return $site_name;
   }
 }
