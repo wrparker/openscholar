97173c5a66caa7063810229b1b30d512e2309f73
ran81 <ran@gizra.com>
2014-07-13 19:25:28 +0800

    Fix so sv_list is not affected.

--- openscholar/themes/os_basetheme/template.php
+++ openscholar/themes/os_basetheme/template.php
@@ -164,8 +164,16 @@ function os_basetheme_preprocess_node(&$vars) {
   // Event nodes, inject variables for date month and day shield
   if ($vars['node']->type == 'event' && !$vars['page']) {
     $vars['event_start'] = array();
+    $delta = 0;
+    if (isset($vars['node']->date_id)) {
+      list(,,, $delta,) = explode('.', $vars['node']->date_id . '.');
+    }
 
-    $delta = $vars['id'];
+    // If the event is not in an sv list we need to update the date id
+    // between function calls to display the correct start date.
+    if (empty($vars['sv_list'])) {
+      $vars['node']->date_id = 'node.' . $vars['node']->nid . '.field_date.' . ($delta + 1);
+    }
     if (isset($vars['field_date'][$delta]['value']) && !empty($vars['field_date'][$delta]['value'])) {
       // Define the time zone in the DB as a UTC.
       $date = new DateTime($vars['field_date'][$delta]['value'], new DateTimeZone('utc'));
