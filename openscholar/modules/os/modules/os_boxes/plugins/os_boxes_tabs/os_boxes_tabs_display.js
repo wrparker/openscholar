2d7aa762d78d304f5ce29a93d340cfffe3b5b358
Matt Petrovic <mpetrovic@iq.harvard.edu>
2014-09-04 10:40:12 -0400

    #6495: Add dedicated wrapper for tabs widget html.

--- openscholar/modules/os/modules/os_boxes/plugins/os_boxes_tabs/os_boxes_tabs_display.js
+++ openscholar/modules/os/modules/os_boxes/plugins/os_boxes_tabs/os_boxes_tabs_display.js
@@ -16,18 +16,19 @@ Drupal.behaviors.os_boxes_tabs = { attach: function (ctx) {
     }
   });
 
-  $('.block-boxes-os_boxes_tabs', ctx).once('tabs', function () {
+  $('.os-boxes-tabbed', ctx).once('tabs', function () {
     // filter out children .block-content elements so we don't make them into tabs too
     // if they are a tab widget (nesting tabs, ugh), then we'll handle them in a separate loop iteration
-    $('.block-content', this).not(this.id + ' .block-content .block-content').tabsBox({
+    var id = $(this).closest('.boxes-box').attr('id');
+    $(this).not('.os-boxes-tabbed .os-boxes-tabbed').tabsBox({
       show: clickHandle,
-      selected: (typeof window.sessionStorage != 'undefined' && typeof sessionStorage[this.id] != 'undefined')?sessionStorage[this.id]:0
+      selected: (typeof window.sessionStorage != 'undefined' && typeof sessionStorage[id] != 'undefined')?sessionStorage[id]:0
     });
   });
 
   function clickHandle(e) {
     var $this = $(this),
-      id = $this.parents('.block').attr('id'),
+      id = $this.closest('.boxes-box').attr('id'),
       val = $(this).tabs('option', 'selected');
 
     if (typeof window.sessionStorage != 'undefined')
