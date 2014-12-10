9707bf6e1f9a8b403efa9006a7c7bd742446fad1
Roy Segall <roy.segall@gizra.com>
2014-12-10 15:01:02 +0200

    Improve the controller.

--- openscholar/modules/os/modules/os_restful/plugins/restful/db_query/spaces/1.0/OsRestfulSpaces.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/db_query/spaces/1.0/OsRestfulSpaces.class.php
@@ -13,7 +13,7 @@ class OsRestfulSpaces extends \RestfulDataProviderDbQuery implements \RestfulDat
   public static function controllersInfo() {
     return array(
       '' => array(
-        \RestfulInterface::GET => 'index',
+        \RestfulInterface::GET => 'getSpace',
         \RestfulInterface::POST => 'createSpace',
         \RestfulInterface::PUT => 'updateSpace',
       ),
@@ -24,7 +24,7 @@ class OsRestfulSpaces extends \RestfulDataProviderDbQuery implements \RestfulDat
    * {@inheritdoc}
    */
   public function publicFieldsInfo() {
-    return $this->simpleFieldsInfo(array('sid', 'type', 'id', 'object_id', 'value'));
+    return $this->simpleFieldsInfo(array('sid', 'type', 'id', 'object_id', 'object_type', 'value'));
   }
 
   /**
@@ -45,16 +45,63 @@ class OsRestfulSpaces extends \RestfulDataProviderDbQuery implements \RestfulDat
   }
 
   /**
+   * Throwing exception easily.
+   * @param $message
+   *   The exception message.
+   * @throws RestfulBadRequestException
+   */
+  public function throwException($message) {
+    throw new RestfulBadRequestException($message);
+  }
+
+  /**
+   * un-serialize the value object.
+   */
+  public function mapDbRowToPublicFields($row) {
+    $row->value = unserialize($row->value);
+    return parent::mapDbRowToPublicFields($row);
+  }
+
+  /**
    * Verify the user's request has access CRUD in the current group.
    */
   public function checkGroupAccess() {
+    // todo: Handle.
+    return TRUE;
+  }
 
+  /**
+   * Override the list method in order to return a specific delta from the
+   * space override.
+   */
+  public function getSpace() {
+    $list = parent::index();
+
+    $request = $this->getRequest();
+
+    if (!empty($request['type']) && !empty($list[0]['value'][$request['type']])) {
+      // We need a sub value from the space.
+      $sub_value = $list[0]['value'][$request['type']];
+
+      if (!empty($request['delta']) && !empty($sub_value[$request['delta']])) {
+        // We need a specif delta from the sub value.
+        return $sub_value[$request['delta']];
+      }
+
+      return $sub_value;
+    }
+
+    return $list;
   }
 
   /**
    * Updating a given space override.
    */
   public function updateSpace() {
+    if (!$this->checkGroupAccess()) {
+      $this->throwException('You are not authorised.');
+    }
+
     $request = $this->getRequest();
   }
 
@@ -62,6 +109,10 @@ class OsRestfulSpaces extends \RestfulDataProviderDbQuery implements \RestfulDat
    * Creating a space override.
    */
   public function createSpace() {
+    if (!$this->checkGroupAccess()) {
+      $this->throwException('You are not authorised.');
+    }
+    
     $request = $this->getRequest();
   }
 }
