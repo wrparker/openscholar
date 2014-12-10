207ba8c8f10dfe5a329dcdb7cf6192cbbe648fee
Roy Segall <roy.segall@gizra.com>
2014-12-10 11:52:45 +0200

    Set up the base for the rest action with spces.

--- openscholar/modules/os/modules/os_restful/plugins/restful/db_query/spaces/1.0/OsRestfulSpaces.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/db_query/spaces/1.0/OsRestfulSpaces.class.php
@@ -8,12 +8,33 @@
 class OsRestfulSpaces extends \RestfulDataProviderDbQuery implements \RestfulDataProviderDbQueryInterface, \RestfulDataProviderInterface {
 
   /**
+   * Overrides \RestfulDataProviderEFQ::controllersInfo().
+   */
+  public static function controllersInfo() {
+    return array(
+      '' => array(
+        \RestfulInterface::GET => 'index',
+        \RestfulInterface::POST => 'createSpace',
+        \RestfulInterface::PUT => 'updateSpace',
+      ),
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function publicFieldsInfo() {
     return $this->simpleFieldsInfo(array('sid', 'type', 'id', 'object_id', 'value'));
   }
 
+  /**
+   * Simple fields mapping.
+   *
+   * @param array $fields
+   *   The list of the properties.
+   * @return array
+   *   List of the schema fields.
+   */
   public function simpleFieldsInfo($fields = array()) {
     $return = array();
     foreach ($fields as $field) {
@@ -21,5 +42,26 @@ class OsRestfulSpaces extends \RestfulDataProviderDbQuery implements \RestfulDat
     }
 
     return $return;
+  }
+
+  /**
+   * Verify the user's request has access CRUD in the current group.
+   */
+  public function checkGroupAccess() {
+
+  }
+
+  /**
+   * Updating a given space override.
+   */
+  public function updateSpace() {
+    $request = $this->getRequest();
+  }
+
+  /**
+   * Creating a space override.
+   */
+  public function createSpace() {
+    $request = $this->getRequest();
   }
 }
