d09021fecc4a23a53a042e879abc7281dbacb9e2
Roy Segall <roy.segall@gizra.com>
2014-12-24 17:00:13 +0200

    Start working on the post for blog and bio.

--- openscholar/modules/os/modules/os_restful/includes/OsNodeRestfulBase.php
+++ openscholar/modules/os/modules/os_restful/includes/OsNodeRestfulBase.php
@@ -2,4 +2,20 @@
 
 class OsNodeRestfulBase extends RestfulEntityBaseNode {
 
+
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['vsite'] = array(
+      'property' => OG_AUDIENCE_FIELD,
+    );
+
+    $public_fields['body'] = array(
+      'property' => 'body',
+      'sub_property' => 'value',
+    );
+
+    return $public_fields;
+  }
+
 }
