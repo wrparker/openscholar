151b25b5e34f368d36810bc248bbd9f36674ed4f
Roy Segall <roy.segall@gizra.com>
2015-01-25 15:34:09 +0200

    Fix os_toggle.js when infinity scroll is initialized.

--- openscholar/modules/os/theme/os_toggle.js
+++ openscholar/modules/os/theme/os_toggle.js
@@ -4,33 +4,34 @@
  *
  */
 (function ($) {
-Drupal.behaviors.os_toggle = {
-  attach: function (ctx) {
-    $('.toggle', ctx).click(function(){
-      $(this).toggleClass("expanded");
-      var slider = null;
+  Drupal.behaviors.os_toggle = {
+    attach: function (ctx) {
+      $('.toggle', ctx).click(function(event) {
+        event.preventDefault();
 
-      $(this).add($(this).parents()).each(function () {
-        var potentials = $(this).siblings('.os-slider');
-        if (potentials.length) {
-          slider = $(potentials[0]);
-        }
-      })
+        $(this).toggleClass("expanded");
+        var slider = null;
 
-      if (!$.browser.msie) {
-        slider.slideToggle("fast");
-      }
-      else {
-        // IE8 Does not work with the slider.
-        if ($(this).hasClass('expanded')) {
-         slider.show();
+        $(this).add($(this).parents()).each(function () {
+          var potentials = $(this).siblings('.os-slider');
+          if (potentials.length) {
+            slider = $(potentials[0]);
+          }
+        });
+
+        if (!$.browser.msie) {
+          slider.slideToggle("fast");
         }
         else {
-          slider.hide();
+          // IE8 Does not work with the slider.
+          if ($(this).hasClass('expanded')) {
+          slider.show();
+          }
+          else {
+            slider.hide();
+          }
         }
-      }
-      return false;
-    });
-  }
-};
+      });
+    }
+  };
 })(jQuery);
