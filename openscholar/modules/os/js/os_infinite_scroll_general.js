1b1e5eb340201d23655b706812bc29dc52028989
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-02-02 15:14:41 -0500

    #6840: Script.

---
+++ openscholar/modules/os/js/os_infinite_scroll_general.js
@@ -0,0 +1,33 @@
+/**
+ * General handlers for all autopagers.
+ */
+(function ($) {
+
+  var loadingAll = false,
+    old_load;
+
+  function load (current, next) {
+    if (old_load) {
+      old_load.call(this, current, next);
+    }
+    // if (next is last) {
+    //  loadingAll = false;
+    // )
+    if (loadingAll) {
+      $.autopager.load();
+    }
+  }
+
+  Drupal.behaviors.osInfiniteScrollGeneral = {
+    attach: function (ctx) {
+      old_load = $.autopager.option('load');
+      $.autopager.option('load', load);
+
+      $('<div class="autopager-load-all"><a>Load All</a></div>').appendTo('#main-content-header').click(function (e) {
+        loadingAll = true;
+        $.autopager.load();
+      });
+    }
+  }
+
+})(jQuery);
