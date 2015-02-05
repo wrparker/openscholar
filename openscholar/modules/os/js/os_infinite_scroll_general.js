6b18a769890ba9fbe0f8318c40697899aa24193c
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-02-05 15:31:30 -0500

    #6840: Replace Load All button with throbber when clicked. Remove all buttons when done.

--- openscholar/modules/os/js/os_infinite_scroll_general.js
+++ openscholar/modules/os/js/os_infinite_scroll_general.js
@@ -13,7 +13,6 @@
     }
     if (!next.page || next.page == NaN) {
       loadingAll = false;
-      $('.autopager-load-all').remove();
     }
     if (loadingAll) {
       setTimeout($.autopager.load, 1);
@@ -24,11 +23,18 @@
     e.preventDefault();
     loadingAll = true;
     $.autopager.load(e);
+
+    var img_path = Drupal.settings.views_infinite_scroll[0].img_path,
+        img = '<div id="views_infinite_scroll-ajax-loader"><img src="' + img_path + '" alt="loading..."/></div>';
+
+    $('#main-content-header .autopager-load-all').replaceWith(img);
+    $('.autopager-load-all').remove();
   }
 
   Drupal.behaviors.osInfiniteScrollGeneral = {
     attach: function (ctx) {
       if (behaviorRun) return;
+      behaviorRun = true;
 
       if (load != $.autopager.option('load')) {
         old_load = $.autopager.option('load');
