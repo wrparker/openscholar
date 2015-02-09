be83aa44b0b19b30040e851e77e4f91e98a152a1
Roy Segall <roy.segall@gizra.com>
2015-02-09 15:18:23 +0200

    Adding bower for download Angualr js components.

--- openscholar/themes/os_basetheme/templates/html.tpl.php
+++ openscholar/themes/os_basetheme/templates/html.tpl.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
-<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?> ng-app><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?> ng-app><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?> ng-app><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>  ng-app><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?> ng-app><!--<![endif]-->
 <head>
 <?php print $head; ?>
 <title><?php print $head_title; ?></title>
