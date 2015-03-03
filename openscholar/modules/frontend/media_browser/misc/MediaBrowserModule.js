50e4973b73c982673dfd392c95ea36f6be2a1dc1
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-03-03 11:13:07 -0500

    Load correct form when selecting file.

--- openscholar/modules/frontend/media_browser/misc/MediaBrowserModule.js
+++ openscholar/modules/frontend/media_browser/misc/MediaBrowserModule.js
@@ -12,10 +12,6 @@
     $scope.selection = 0;
     $scope.selection_form = '';
 
-        $scope.getId = function () {
-          return $scope.$id;
-        }
-
     // Watch for changes in file list
     $scope.$on('EntityService.files.add', function (event, files) {
       $scope.files = files;
@@ -29,27 +25,17 @@
       $scope.numFiles = service.getCount();
     });
 
-    // Filter list of files by a filename fragment
-    $scope.queryFilename = function (item, index) {
-
-      if ($scope.search) {
-        var search = $scope.search.toLowerCase();
-        if (item.name.toLowerCase().indexOf(search) > -1) {
-          return true;
-        }
-        else if (item.orig && item.orig.toLowerCase().indexOf(search) > -1) {
-          return true;
-        }
-        return false;
-      }
-      return true;
-    };
-
     // selected file
     $scope.setSelection = function (fid) {
       $scope.selection = fid;
-      $scope.selected_file = angular.copy(FileService.get(fid));
-      $scope.selection_form = rootPath+'/templates/'+$scope.selected_file.form+'.html';
+      $scope.selected_file = angular.copy(service.get(fid));
+      switch ($scope.selected_file.type) {
+        case 'image':
+          $scope.selection_form = rootPath+'/templates/file_edit_image.html';
+          break;
+        default:
+          $scope.selection_form = rootPath+'/templates/file_edit_default.html';
+      }
     };
 
     // preload file edit templates
@@ -58,11 +44,11 @@
 
     // file edit form methods
     $scope.save = function() {
-      FileService.edit(selected_file);
+      service.edit($scope.selected_file);
     };
 
-    $scope.delete = function() {
-      FileService.delete(selected_file);
+    $scope.deleteConfirmed = function() {
+      service.delete($scope.selected_file);
     };
   }]);
 })();
