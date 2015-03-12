b959c16a6764aa2a23c9b0410461a1a2c9c401bc
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-03-12 16:10:11 -0400

    #7024: Add file handling to rest api handler.

--- openscholar/modules/os/modules/os_restful/plugins/restful/files/1.0/OsFilesResource.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/files/1.0/OsFilesResource.class.php
@@ -78,4 +78,44 @@ class OsFilesResource extends RestfulEntityBase {
     $output = file_view($wrapper->value(), 'preview');
     return drupal_render($output);
   }
+
+  /**
+   * Override. Handle the file upload process before creating an actual entity.
+   * The file could be a straight replacement, and this is where we handle that.
+   */
+  public function createEntity() {
+    if ($this->checkEntityAccess('create', 'file', NULL) === FALSE) {
+      // User does not have access to create entity.
+      $params = array('@resource' => $this->getPluginKey('label'));
+      throw new RestfulForbiddenException(format_string('You do not have access to create a new @resource resource.', $params));
+    }
+
+    $destination = 'public://';
+    // do spaces/private file stuff here
+    if ($this->request['vsite']) {
+      $path = db_select('purl', 'p')->fields('p', array('value'))->condition('id', $this->request['vsite'])->execute()->fetchField();
+      $destination .= $path.'/files';
+    }
+    if ($entity = file_save_upload('upload', array(), $destination, FILE_EXISTS_REPLACE)) {
+
+      if ($this->request['vsite']) {
+        og_group('node', $this->request['vsite'], array('entity_type' => 'file', 'entity' => $entity));
+      }
+
+      $entity->status = FILE_STATUS_PERMANENT;
+      $entity = file_save($entity);
+
+      $wrapper = entity_metadata_wrapper($this->entityType, $entity);
+
+      $this->setPropertyValues($wrapper);
+      return array($this->viewEntity($wrapper->getIdentifier()));
+    }
+    elseif ($_FILES['files']['errors']['upload']) {
+      throw new RestfulUnprocessableEntityException('Error uploading new file to server.');
+    }
+    else {
+      // probably a url or html snippet
+    }
+  }
+
 }
