3e9cc5593173d939c48d9d2dc2ab290f64595e4c
Roy Segall <roy.segall@gizra.com>
2015-04-08 14:47:28 +0300

    Add terms field to the file object.

--- openscholar/modules/os/modules/os_restful/plugins/restful/files/1.0/OsFilesResource.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/files/1.0/OsFilesResource.class.php
@@ -8,38 +8,52 @@ class OsFilesResource extends RestfulEntityBase {
   public function publicFieldsInfo() {
     $info = parent::publicFieldsInfo();
 
-    $info += array(
-      'size' => array(
-        'property' => 'size',
+    $info['size'] = array(
+      'property' => 'size',
+    );
+
+    $info['mimetype'] = array(
+      'property' => 'mime',
+    );
+
+    $info['url'] = array(
+      'property' => 'url',
+    );
+
+    $info['type'] = array(
+      'property' => 'type',
+    );
+
+    $info['name'] = array(
+      'property' => 'name',
+    );
+
+    $info['timestamp'] = array(
+      'property' => 'size',
+    );
+
+    $info['description'] = array(
+      'property' => 'os_file_description',
+      'sub_property' => 'value',
+    );
+
+    $info['image_alt'] = array(
+      'callback' => array($this, 'getImageAltText'),
+    );
+
+    $info['image_title'] = array(
+      'callback' => array($this, 'getImageTitleText'),
+    );
+
+    $info['preview'] = array(
+      'callback' => array($this, 'getFilePreview'),
+    );
+
+    $info['terms'] = array(
+      'property' => OG_VOCAB_FIELD,
+      'process_callbacks' => array(
+        array($this, 'processOgVocabField'),
       ),
-      'mimetype' => array(
-        'property' => 'mime',
-      ),
-      'url' => array(
-        'property' => 'url',
-      ),
-      'type' => array(
-        'property' => 'type',
-      ),
-      'name' => array(
-        'property' => 'name',
-      ),
-      'timestamp' => array(
-        'property' => 'timestamp',
-      ),
-      'description' => array(
-        'property' => 'os_file_description',
-        'sub_property' => 'value',
-      ),
-      'image_alt' => array(
-        'callback' => array($this, 'getImageAltText'),
-      ),
-      'image_title' => array(
-        'callback' => array($this, 'getImageTitleText'),
-      ),
-      'preview' => array(
-        'callback' => array($this, 'getFilePreview'),
-      )
     );
 
     return $info;
@@ -50,10 +64,12 @@ class OsFilesResource extends RestfulEntityBase {
    */
   private function getBundleProperty($wrapper, $field) {
     $properties = $wrapper->getPropertyInfo();
+
     if (isset($properties[$field])) {
       $property = $wrapper->get($field);
       return $property->value();
     }
+
     return null;
   }
 
@@ -77,5 +93,25 @@ class OsFilesResource extends RestfulEntityBase {
   public function getFilePreview($wrapper) {
     $output = file_view($wrapper->value(), 'preview');
     return drupal_render($output);
+  }
+
+  /**
+   * Process the og vocab field.
+   * 
+   * @param $terms
+   * @return array
+   */
+  public function processOgVocabField($terms) {
+    $return = array();
+
+    foreach ($terms as $terms) {
+      $return[] = array(
+        'id' => $terms->tid,
+        'label' => $terms->name,
+        'vid' => $terms->vid,
+      );
+    }
+
+    return $return;
   }
 }
