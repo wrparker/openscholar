7fe5a0531f3a99edad712b1d35543b48db21d2c5
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-04-13 16:01:42 -0400

    #7024: Taxonomy widget internals and template.

--- openscholar/modules/frontend/os_common/TaxonomyWidget/TaxonomyWidget.module.js
+++ openscholar/modules/frontend/os_common/TaxonomyWidget/TaxonomyWidget.module.js
@@ -7,19 +7,32 @@
     .directive('taxonomyWidget', ['EntityService', function (EntityService) {
       return {
         scope: {
-          vocabId: '@',
           terms: '='
         },
         template: '<div class="tw-widget"><div ng-include="widgetType"></div></div>',
         link: function (scope, elem, attrs, c, trans) {
-          var vocabService = new EntityService('vocabulary', 'id'),
-            vocab = vocabService.get(scope.vocabId),
+          var entityType = attrs.entitytype,
+            entityBundle = attrs.bundle,
+            vocabService = new EntityService('vocabulary', 'id'),
             termService = new EntityService('terms', 'id'),
             terms = termService.getAll({vocab: scope.vocabId});
 
-          scope.widgetType = vocab.something;
-          scope.allTerms = [];
-          scope.selectedTerms = {};
+          vocabService.fetch({entity_type: entityType, bundle: entityBundle})
+            .then(function (result) {
+              console.log(result);
+              scope.vocabs = result;
+              for (var i=0; i<scope.vocabs.length; i++) {
+                termService.fetch({vocab: scope.vocabs[i].id})
+                  .then(function (result) {
+                    console.log(result);
+                    for (var j=0; j<scope.vocabs.length; j++) {
+
+                    }
+                  });
+              }
+            });
+
+
         }
       }
     }])
