ac17823793ced535dbb76bd4a6bda1a13f4f5ac2
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-04-15 11:18:20 -0400

    #7024: Placeholders for all the widget types.

--- openscholar/modules/frontend/os_common/TaxonomyWidget/TaxonomyWidget.module.js
+++ openscholar/modules/frontend/os_common/TaxonomyWidget/TaxonomyWidget.module.js
@@ -16,29 +16,31 @@
           var entityType = attrs.entityType,
             vocabService = new EntityService('vocabulary', 'id'),
             termService = new EntityService('taxonomy', 'id');
-          console.log(scope);
+
+          scope.allTerms = {};
 
           attrs.$observe('bundle', function (value) {
-            console.log(value);
-
             if (!value) return;
+
             vocabService.fetch({entity_type: entityType, bundle: value})
               .then(function (result) {
-                console.log(result);
                 if (!(result.status == 200 && result.statusText == "OK")) {
                   return;
                 }
 
                 scope.vocabs = result.data.data;
                 for (var i=0; i<scope.vocabs.length; i++) {
+                  scope.allTerms[scope.vocabs[i].id] = [];
                   termService.fetch({vocab: scope.vocabs[i].id})
                     .then(function (result) {
-                      console.log(result);
-                      for (var j=0; j<scope.vocabs.length; j++) {
-
+                      for (var j=0; j<result.data.data.length; j++) {
+                        var t = result.data.data[j];
+                        scope.allTerms[parseInt(t.vid)].push(t);
                       }
                     });
                 }
+
+                console.log(scope.allTerms);
               });
           });
         }
