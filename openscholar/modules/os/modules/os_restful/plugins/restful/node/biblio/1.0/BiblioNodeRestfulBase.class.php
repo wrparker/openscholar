5efddb8acd6593feca3dd93acd558934d74ba17b
Roy Segall <roy.segall@gizra.com>
2015-05-17 14:35:57 +0300

    Adding more doc.

--- openscholar/modules/os/modules/os_restful/plugins/restful/node/biblio/1.0/BiblioNodeRestfulBase.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/node/biblio/1.0/BiblioNodeRestfulBase.class.php
@@ -3,10 +3,10 @@
 class BiblioNodeRestfulBase extends OsNodeRestfulBase {
 
   /**
-   * @api {get} /biblio/:id Biblio
+   * @api {get} /biblio/:id Get
    * @apiVersion 0.1.0
-   * @apiName GetEndpoints
-   * @apiGroup Content
+   * @apiName Get
+   * @apiGroup Publication
    *
    * @apiDescription Consume publications content.
    *
@@ -27,5 +27,74 @@ class BiblioNodeRestfulBase extends OsNodeRestfulBase {
     );
 
     return $public_fields;
+  }
+
+  /**
+   * @api {post} /biblio Post
+   * @apiVersion 0.1.0
+   * @apiName Post
+   * @apiGroup Publication
+   *
+   * @apiDescription Create a Publication entry.
+   *
+   * @apiParam {Number} id The publication ID
+   *
+   * @apiSampleRequest off
+   *
+   * @apiSuccess {Number}   id        The publication ID.
+   * @apiSuccess {String}   label     Registration Date.
+   * @apiSuccess {Integer}  vsite     The vsite ID.
+   * @apiSuccess {string}   body      The body of the publication.
+   * @apiSuccess {Integer[]} files    List of file IDs.
+   * @apiSuccess {Integer}  type      The publication type ID.
+   */
+  public function createEntity() {
+    return parent::createEntity();
+  }
+
+  /**
+   * @api {delete} /biblio/:id Delete
+   * @apiVersion 0.1.0
+   * @apiName Delete
+   * @apiGroup Publication
+   *
+   * @apiDescription Create a Publication entry.
+   *
+   * @apiParam {Number} id The publication ID
+   *
+   * @apiSampleRequest off
+   *
+   * @apiSuccess {Number}   id        The publication ID.
+   * @apiSuccess {String}   label     Registration Date.
+   * @apiSuccess {Integer}  vsite     The vsite ID.
+   * @apiSuccess {string}   body      The body of the publication.
+   * @apiSuccess {Integer[]} files    List of file IDs.
+   * @apiSuccess {Integer}  type      The publication type ID.
+   */
+  public function deleteEntity($entity_id) {
+    parent::deleteEntity($entity_id);
+  }
+
+  /**
+   * @api {patch} /biblio/:id Patch
+   * @apiVersion 0.1.0
+   * @apiName Patch
+   * @apiGroup Publication
+   *
+   * @apiDescription Create a Publication entry.
+   *
+   * @apiParam {Number} id The publication ID
+   *
+   * @apiSampleRequest off
+   *
+   * @apiSuccess {Number}   id        The publication ID.
+   * @apiSuccess {String}   label     Registration Date.
+   * @apiSuccess {Integer}  vsite     The vsite ID.
+   * @apiSuccess {string}   body      The body of the publication.
+   * @apiSuccess {Integer[]} files    List of file IDs.
+   * @apiSuccess {Integer}  type      The publication type ID.
+   */
+  public function patchEntity($entity_id) {
+    parent::patchEntity($entity_id);
   }
 }
