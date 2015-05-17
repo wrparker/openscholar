ac0af867e34798a758575b7e3b386eb10338aa69
Roy Segall <roy.segall@gizra.com>
2015-05-17 11:47:11 +0300

    Adding integration to API doc.

--- openscholar/modules/os/modules/os_restful/plugins/restful/node/biblio/1.0/BiblioNodeRestfulBase.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/node/biblio/1.0/BiblioNodeRestfulBase.class.php
@@ -2,6 +2,23 @@
 
 class BiblioNodeRestfulBase extends OsNodeRestfulBase {
 
+  /**
+   * @api {get} /biblio/:id Biblio
+   * @apiVersion 0.1.0
+   * @apiName GetEndpoints
+   * @apiGroup Content
+   *
+   * @apiDescription Consume publications content.
+   *
+   * @apiParam {Number} id The publication ID
+   *
+   * @apiSuccess {Number}   id        The publication ID.
+   * @apiSuccess {String}   label     Registration Date.
+   * @apiSuccess {Object}   vsite     The vsite object.
+   * @apiSuccess {string}   body      The body of the publication.
+   * @apiSuccess {Object[]} files     The attached files.
+   * @apiSuccess {Integer}  type      The publication type ID.
+   */
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
