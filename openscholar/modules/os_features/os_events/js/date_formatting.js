e60b89487ffcbc03808bfc8391993fe2ef8a869a
Roy Segall <roy.segall@gizra.com>
2015-06-22 16:05:27 +0300

    Revert "Minor improvement."
    This reverts commit 394016b22418aa5333083e45eeb08c22d9214f67.

--- openscholar/modules/os_features/os_events/js/date_formatting.js
+++ openscholar/modules/os_features/os_events/js/date_formatting.js
@@ -28,14 +28,14 @@
       html = up;
 
       for (var day in days) {
-        html += '<div class="days-' + day + '-wrapper" instance_delta=0><strong>' + day + '</strong>: ';
+        html += '<div class="days-' + days[day] + '-wrapper"><strong>' + day + '</strong>: ';
         //html += '<a href="#" class="days-' + days[day] + '-prev hide"> << </a>';
         for (var instance in days[day]) {
           if (instance == 0) {
             html += days[day][instance];
           }
         }
-        html += '<a href="#" class="days-' + day + '-next"> >> </a>';
+        html += '<a href="#" class="days-' + days[day] + '-next"> >> </a>';
         html += '</div>';
       }
 
