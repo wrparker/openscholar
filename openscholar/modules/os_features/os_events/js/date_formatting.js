da63da8f1e83a69bb471b5c71148aa40c934fcb6
Roy Segall <roy.segall@gizra.com>
2015-06-22 15:44:51 +0300

    First version of the js to improve the UX for a lot of repeating events date.

---
+++ openscholar/modules/os_features/os_events/js/date_formatting.js
@@ -0,0 +1,46 @@
+(function ($) {
+
+  /**
+   * Format repeating events.
+   */
+  Drupal.behaviors.OsEventsRepeatingDatesFormatting = {
+    attach: function () {
+      var days = [];
+      var up;
+      $('#main-content .field-type-datetime .field-items .field-item').each(function(index) {
+        if (index == 0) {
+          up = $(this).text();
+          return -1;
+        }
+
+        var text = $(this).text();
+        var split = text.split(', ');
+        var day = split[0];
+
+        if (days[day] === undefined) {
+          days[day] = [];
+        }
+
+        days[day].push(text.replace(day + ', ', ''));
+      });
+      var html;
+
+      html = up;
+
+      for (var day in days) {
+        html += '<div class="days-' + days[day] + '-wrapper"><strong>' + day + '</strong>: ';
+        //html += '<a href="#" class="days-' + days[day] + '-prev hide"> << </a>';
+        for (var instance in days[day]) {
+          if (instance == 0) {
+            html += days[day][instance];
+          }
+        }
+        html += '<a href="#" class="days-' + days[day] + '-next"> >> </a>';
+        html += '</div>';
+      }
+
+      $('#main-content .field-type-datetime .field-items').html(html);
+    }
+  };
+
+})(jQuery);
