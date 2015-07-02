fb0da6a04691539f0fc760fb45db217374de7834
Roy Segall <roy.segall@gizra.com>
2015-07-02 15:45:09 +0300

    Add missing fields to the slide show end point.

--- openscholar/modules/os/modules/os_restful/plugins/restful/node/slideshow_slide/1.0/SlideshowSlideNodeRestfulBase.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/node/slideshow_slide/1.0/SlideshowSlideNodeRestfulBase.class.php
@@ -5,6 +5,33 @@ class SlideshowSlideNodeRestfulBase extends OsNodeRestfulBase {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['description'] = array(
+      'property' => 'field_description',
+    );
+
+    $public_fields['link'] = array(
+      'property' => 'field_link',
+    );
+
+    $public_fields['headline'] = array(
+      'property' => 'field_headline',
+    );
+
+    $public_fields['alt_text'] = array(
+      'property' => 'field_slideshow_alt_text',
+    );
+
+    $public_fields['title_text'] = array(
+      'property' => 'field_slideshow_title_text',
+    );
+
+    $public_fields['image'] = array(
+      'property' => 'field_image',
+      'process_callbacks' => array(
+        array($this, 'singleFileFieldDisplay'),
+      ),
+    );
+
     return $public_fields;
   }
 
