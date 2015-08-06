467a7f65907fe936a257d85644a28014c8b69f0d
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-08-06 15:24:46 -0400

    #7024: Remove unused files.

--- openscholar/modules/frontend/media_browser/misc/MediaBrowserDirectives.js
+++
@@ -1,8 +0,0 @@
-(function() {
-var rootPath = Drupal.settings.osRestModulePath,
-    restPath = Drupal.settings.restBasePath;
-
-angular.module('mediaBrowser.directives', ['angularModalService']);
-
-
-})();
