af2e0d11831b3917ecd783abd138d543f7466eb3
Roy Segall <roy.segall@gizra.com>
2015-08-10 16:01:49 +0300

    First work on the menu end point.

---
+++ openscholar/modules/os/modules/os_restful/plugins/restful/menu/1.0/OsRestfulMenu.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \OsRestfulMenu
+ */
+
+class OsRestfulMenu extends \OsRestfulDataProvider {
+
+  /**
+   * Return the properties that should be public.
+   *
+   * @throws \RestfulEntityViewMode
+   *
+   * @return array
+   */
+  public function publicFieldsInfo() {
+    $fields = array();
+
+    $properties = array(
+      'id' => 'mlid',
+      'path' => 'link_path',
+      'title' => 'link_title',
+    );
+
+    foreach ($properties as $key => $property) {
+      $fields[$key] = array(
+        'property' => $property,
+      );
+    }
+
+    return $fields;
+  }
+}
