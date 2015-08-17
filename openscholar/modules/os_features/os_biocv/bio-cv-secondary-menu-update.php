754e65de234952138a923aaff04dafa75298b62c
bilsi <bbalakrishnan@dvt9m32.AD.FAS.HARVARD.EDU>
2015-08-17 12:13:59 -0400

    added php file

---
+++ openscholar/modules/os_features/os_biocv/bio-cv-secondary-menu-update.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bbalakrishnan
+ * Date: 8/17/15
+ * Time: 11:24 AM
+ */
+
+$result = db_query("SELECT id from spaces_overrides WHERE object_id = 'secondary_menu' AND object_type = 'menu' AND value like '%biocv%';");
+foreach ($result as $id => $id) {
+{
+    $vsite_obj = vsite_get_vsite($id);
+}
+
+// Get secondary menu info for bio & cv links
+$secondary_menu = array();
+foreach (_vsite_menu_get_menu_links('secondary-menu',  $vsite_obj) as $mlid => $menu_item) {
+  if (isset($menu_item['link_path']) && ($menu_item['link_path'] == "biocv" || $menu_item['link_path'] == "biocv/cv")) {
+    $secondary_menu[$menu_item['link_path']] = $menu_item;
+  }
+}
+
+// create new primary menu item(s) for bio/cv page node(s)
+if (count($secondary_menu)) {
+  foreach ($secondary_menu as $type => $menu) {
+    $new_menu = array(
+      "menu_name" => 'secondary-menu',
+      "router_path" => "node/%",
+      "link_title" => $menu['link_title'],
+      "options" => $menu['options'],
+      "weight" => $menu['weight'],
+      "module" => 'system',
+      "hidden" => $menu['hidden'],
+      "external" => $menu['external'],
+      "plid" => $menu['plid'],
+    );
+    if ($type == "biocv") {
+      $new_menu['link_path'] = 'node/' . $bio_node->nid;
+      $mlid = vsite_menu_menu_link_save($new_menu, $gid);
+    }
+    elseif ($type == "biocv/cv") {
+      $new_menu['link_path'] = 'node/' . $cv_node->nid;
+      $mlid = vsite_menu_menu_link_save($new_menu, $gid);
+    }
+  }
+  vsite_menu_cache_clear('secondary-menu', $gid);
+}
