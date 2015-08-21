25b1449d3724cf907020595b70188eedd531cc44
bilsi <bbalakrishnan@dvt9m32.AD.FAS.HARVARD.EDU>
2015-08-21 10:38:44 -0400

    updating the biocv script

--- openscholar/modules/os_features/os_biocv/bio-cv-secondary-menu-update.php
+++ openscholar/modules/os_features/os_biocv/bio-cv-secondary-menu-update.php
@@ -24,31 +24,46 @@ foreach ($results as $id) {
       $secondary_menu[$menu_item['link_path']] = $menu_item;
     }
   }
-}
 
-// create new primary menu item(s) for bio/cv page node(s)
-if (count($secondary_menu)) {
-  foreach ($secondary_menu as $type => $menu) {
-    $new_menu = array(
-      "menu_name" => 'secondary-menu',
-      "router_path" => "node/%",
-      "link_title" => $menu['link_title'],
-      "options" => $menu['options'],
-      "weight" => $menu['weight'],
-      "module" => 'system',
-      "hidden" => $menu['hidden'],
-      "external" => $menu['external'],
-      "plid" => $menu['plid'],
-    );
-    if ($type == "biocv") {
-      $new_menu['link_path'] = 'node/' . $bio_node->nid;
-      $mlid = vsite_menu_menu_link_save($new_menu, $gid);
+  // create new primary menu item(s) for bio/cv page node(s)
+  if (count($secondary_menu)) {
+    foreach ($secondary_menu as $type => $menu) {
+      $new_menu = array(
+        "menu_name" => 'secondary-menu',
+        "router_path" => "node/%",
+        "link_title" => $menu['link_title'],
+        "options" => $menu['options'],
+        "weight" => $menu['weight'],
+        "module" => 'system',
+        "hidden" => $menu['hidden'],
+        "external" => $menu['external'],
+        "plid" => $menu['plid'],
+      );
+
+      $vsite_name = vsite_get_purl($vsite_obj);
+
+      if ($type == "biocv") {
+        $bio_node = db_select('url_alias')
+            ->fields('source')
+            ->condition('alias', $vsite_name.'/biocv%', 'LIKE')
+            ->execute();
+        $bio_node = substr($bio_node, 6, strlen($bio_node));
+
+        $new_menu['link_path'] = 'node/' . $bio_node->nid;
+        $mlid = vsite_menu_menu_link_save($new_menu, $id);
+      }
+      elseif ($type == "biocv/cv") {
+        $cv_node = db_select('url_alias')
+           ->fields('source')
+           ->condition('alias', $vsite_name.'/biocv/cv%', 'LIKE')
+           ->execute();
+        $cv_node = substr($bio_node, 6, strlen($cv_node));
+
+        $new_menu['link_path'] = 'node/' . $cv_node->nid;
+        $mlid = vsite_menu_menu_link_save($new_menu, $id);
+      }
     }
-    elseif ($type == "biocv/cv") {
-      $new_menu['link_path'] = 'node/' . $cv_node->nid;
-      $mlid = vsite_menu_menu_link_save($new_menu, $gid);
-    }
+    vsite_menu_cache_clear('secondary-menu', $id);
   }
-  vsite_menu_cache_clear('secondary-menu', $gid);
 }
 ?>
