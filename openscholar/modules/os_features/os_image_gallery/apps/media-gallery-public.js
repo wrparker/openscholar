fe2878451746d0a5a741168a27bd272abe688dd2
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-08-25 17:01:56 -0400

    #7380: Pass media gallery file size restriction to Media Browser.

--- openscholar/modules/os_features/os_image_gallery/apps/media-gallery-public.js
+++ openscholar/modules/os_features/os_image_gallery/apps/media-gallery-public.js
@@ -15,5 +15,30 @@
         fid: urlBits[1]
       });
     }
+
+    if (Drupal && Drupal.media_gallery) {
+      jQuery('a.media-gallery-add.launcher').unbind('click', Drupal.media_gallery.open_browser);
+
+      // Mostly copied from media_gallery.addimage.js
+      // We just need to force the file size settings to be passed to the media browser
+      Drupal.media_gallery.open_browser = function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        // get node id for the link that was clicked and save to settings (id attribute for parent article is node-[id])
+        Drupal.settings.mediaGalleryNodeNid = jQuery(this).closest("article.contextual-links-region").attr('id').substring(5);
+
+        var pluginOptions = {
+          'id': 'media_gallery',
+          'multiselect' : true ,
+          'types': Drupal.settings.mediaGalleryAllowedMediaTypes,
+          max_filesize: Drupal.settings.mediaGalleryMaxFilesize,
+          max_filesize_raw: Drupal.settings.mediaGalleryMaxFilesizeRaw
+        };
+        Drupal.media.popups.mediaBrowser(Drupal.media_gallery.add_media, pluginOptions);
+      }
+
+      jQuery('a.media-gallery-add.launcher.media-gallery-add-processed').bind('click', Drupal.media_gallery.open_browser);
+    }
   });
 })();
