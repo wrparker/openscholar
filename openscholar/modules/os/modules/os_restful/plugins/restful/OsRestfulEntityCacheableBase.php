bf8b592d5552fbead3197bba7ca0a3a7a8b05725
Matt Petrovic <mpetrovic@iq.harvard.edu>
2015-09-25 14:09:24 -0400

    #7511: Return updated entities when given a timestamp.

--- openscholar/modules/os/modules/os_restful/plugins/restful/OsRestfulEntityCacheableBase.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/OsRestfulEntityCacheableBase.php
@@ -21,7 +21,56 @@ class OsRestfulEntityCacheableBase extends RestfulEntityBase {
    */
   public function getUpdates($path) {
     $timestamp = str_replace('updates/', '', $path);
-    return array($timestamp);
+
+    if ($timestamp < strtotime('-30 days')) {
+      return $this->getList();
+    }
+
+    $request = $this->getRequest();
+
+    $entity_type = $this->entityType;
+    $result = $this
+      ->getQueryForUpdates($timestamp)
+      ->execute();
+
+    $return = array();
+    if (!empty($result[$entity_type])) {
+      $ids = array_keys($result[$entity_type]);
+
+      // Pre-load all entities if there is no render cache.
+      $cache_info = $this->getPluginKey('render_cache');
+      if (!$cache_info['render']) {
+        entity_load($entity_type, $ids);
+      }
+
+      $return = array();
+
+      // If no IDs were requested, we should not throw an exception in case an
+      // entity is un-accessible by the user.
+      foreach ($ids as $id) {
+        if ($row = $this->viewEntity($id)) {
+          $return[] = $row;
+        }
+      }
+    }
+
+    return $return;
+  }
+
+  public function getQueryForUpdates($timestamp) {
+    $info = $this->getEntityInfo();
+    $entity_type = $this->getEntityType();
+    $query = $this->getEntityFieldQuery();
+
+    $this->queryForListSort($query);
+    $this->queryForListFilter($query);
+    $this->queryForListPagination($query);
+    $this->addExtraInfoToQuery($query);
+
+    if (in_array('changed', $info['schema_fields_sql']['base table'])) {
+      $query->propertyCondition('changed', (int)$timestamp, '>');
+    }
+    return $query;
   }
 }
 
