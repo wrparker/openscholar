f75d26824a15989b85e2483213922276151ee3e8
Rebecca Asch <rsasch@speakeasy.net>
2015-11-18 13:12:20 -0500

    set up function for site report CSV download

--- openscholar/modules/os_features/os_reports/js/ReportModule.js
+++ openscholar/modules/os_features/os_reports/js/ReportModule.js
@@ -1,9 +1,11 @@
 (function () {
-	angular.module('osReports', [])
-		.controller('SiteReport', ['$data', function($data) {
-			$data.update = function(query) {
-				
-			};
-		}]);
-
+  angular.module('ReportModule', [])
+    .controller('SiteReport', [$http, function($http) {
+      this.update = function(query) {
+        var apiUrl = '/api/report_sites/attributes';
+        $http({method : 'get', url: apiUrl, params: query}).
+          then(function(response) {
+          });         
+      };
+    }]);
 })();
