1f60def28aa294a9c4f82517fd90b9e4638ec86f
Rebecca Asch <rsasch@speakeasy.net>
2015-11-19 17:05:29 -0500

    first stab at angular for site report

--- openscholar/modules/os_features/os_reports/js/ReportModule.js
+++ openscholar/modules/os_features/os_reports/js/ReportModule.js
@@ -1,8 +1,34 @@
 (function () {
   var reportModule = angular.module('ReportModule', []);
-  reportModule.controller('SiteReport', function ($scope) {
+  reportModule.controller('SiteReportQuery', ['$scope','$http', function ($scope, $http) {
+
     $scope.update = function(query) {
-      // update results table
-    };
-  });
+      var queryString = "?" + jQuery.param(query);
+
+      console.log(queryString);
+
+      var request = {
+        method: 'GET',
+        url : 'http://rsa.hwpi.harvard.edu/api/report_sites/attributes' + queryString,
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        data : query,
+      };
+
+      $http(request).then(function(response) {
+        $scope.headers = [];
+
+        var responseData = angular.fromJson(response.data.data);
+        var key;
+        for (key in responseData[0]) {
+          $scope.headers.push(key);
+        }
+        $scope.rows = responseData;
+      }, 
+      // error
+      function() {
+      });
+    };    
+  }]);
 })();
