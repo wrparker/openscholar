b0b31b2736cf7687e7317338d7dbd8080c484f2e
Rebecca Asch <rsasch@speakeasy.net>
2015-12-08 17:02:22 -0500

    added pagination, dynamic range and spinner

--- openscholar/modules/os_features/os_reports/js/ReportModule.js
+++ openscholar/modules/os_features/os_reports/js/ReportModule.js
@@ -1,6 +1,12 @@
 (function () {
   var reportModule = angular.module('ReportModule', ['os-auth']);
   reportModule.controller('SiteReportQuery', ['$http', '$scope', function ($http, $scope) {
+    $scope.pager = function($direction) {
+      var url = '';
+      eval ('url = $scope.' + $direction + ';');
+      $scope.params = convertRestURLtoObj(url);
+      $scope.update();
+    }
 
     $scope.updateCheckedValues = function($set, $value) {
       if (!$scope.params) {
@@ -41,9 +47,14 @@
       }
     };
 
-    $scope.update = function update($query) {
+    $scope.update = function update() {
+      // reset values
       $scope.headers = [];
       $scope.rows = [];
+      jQuery("div.results").css("background-image", "url('/profiles/openscholar/modules/frontend/os_common/FileEditor/large-spin_loader.gif')");
+      jQuery(".pager a").hide();
+      $scope.status = "";
+      $scope.params.range = $scope.query.limit;
 
       if ($scope.params && $scope.params.lastupdate) {
         $scope.params.exclude = ['feed_importer', 'profile', 'harvard_course'];
@@ -51,13 +62,34 @@
 
       var $request = {
         method: 'POST',
-        url : Drupal.settings.paths.api + '/report_sites/attributes',
+        url : Drupal.settings.paths.api + '/report_sites',
         headers : {'Content-Type' : 'application/json'},
         data : $scope.params,
       };
 
       $http($request).then(function($response) {
         var $responseData = angular.fromJson($response.data.data);
+
+        $scope.total = $response.data.count;
+
+        if ($response.data.next != null) {
+          $scope.next = $response.data.next.href;
+          jQuery(".pager .next a").show();
+        }
+        else {
+          $scope.next = null;
+          jQuery(".pager .next a").hide();
+        }
+
+        if ($response.data.previous != null) {
+          $scope.previous = $response.data.previous.href;
+          jQuery(".pager .previous a").show();
+        }
+        else {
+          $scope.previous = null;
+          jQuery(".pager .previous a").hide();
+        }
+
         var $keys = [];
 
         // get table headers from returned data
@@ -67,9 +99,27 @@
           }
         }
         $scope.rows = $responseData;
-      }, 
+
+        if ($scope.params.page == null) {
+          $scope.params.page = 1;
+        }
+
+        if (($scope.params.page * $scope.params.range) < $scope.total) {
+          $end = ($scope.params.page * $scope.params.range);
+        }
+        else {
+          $end = $scope.total;
+        }
+
+        if ($scope.total) {
+          $scope.status = "showing " + ((($scope.params.page - 1) * $scope.params.range) + 1) + " - " + $end + " of " + $scope.total;
+        }
+      },
       // error
       function() {
+        jQuery("div.results").css("background-image", "none");
+        ajax_command_remove('#messages');
+        ajax_command_after('#header-region', '<div id="messages">Something went wrong.</div>');
       });
     };    
 
@@ -81,7 +131,10 @@
         $scope.params.sort = $obj.header;
       }
 
-      $scope.update($scope.query);
+      // reset to page 1
+      $scope.params.page = '1';
+
+      $scope.update();
     };
   }]);
 
@@ -107,13 +160,39 @@
     };
   });
 
-  var PagingModule = angular.module('PagingModule', ['ui.bootstrap']);
+  // function to take URL and return a javascript object of the query string
+  var convertRestURLtoObj = function (url) {
+      var query = decodeURIComponent(url.split('?').slice(1).toString());
+      $queryArray = query.split('&');
+      $queryObj = new Object();
 
-  PagingModule.controller('PaginationCtrl', function ($scope) {
-    $scope.currentPage = 1;
-    $scope.setPage = function () {
-    };
-  });
+      jQuery.each($queryArray, function($key, $value) {
+        $pair = $value.split('=');
+        $arrayFlag = 0;
+        if ($pair[0].match(/\[\d+\]/)) {
+          $arrayFlag = 1;
+          $pair[0] = $pair[0].replace(/\[\d+\]/g, '');
+        }
+        if ($queryObj[$pair[0]]) {
+          if (typeof $queryObj[$pair[0]] === "string") {
+            $queryObj[$pair[0]] = new Array($queryObj[$pair[0]], $pair[1]);
+          }
+          else {
+            $queryObj[$pair[0]].push($pair[1]);
+          }
+        }
+        else {
+          if ($arrayFlag) {
+            $queryObj[$pair[0]] = new Array($pair[1]);
+          }
+          else {
+            $queryObj[$pair[0]] = $pair[1];
+          }
+        }
+      });
+
+      return $queryObj;
+  };
 
 
 })();
