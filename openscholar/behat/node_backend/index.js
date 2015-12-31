3621b35cf51ccc9821b8eadbce40155eea9b9690
Roy Segall <roy.segall@gizra.com>
2015-12-31 19:04:55 +0200

    Start work on misc.

--- openscholar/behat/node_backend/index.js
+++ openscholar/behat/node_backend/index.js
@@ -4,7 +4,11 @@ var fs = require('fs');
 express()
   .get('*', function(req, response) {
     fs.readFile(__dirname + '/public/' + req.query.file, 'utf8', function (err, data) {
-      response.set('Content-Type', 'application/xml; charset=utf-8');
+
+      if (req.query.file == 'courses.xml') {
+        response.set('Content-Type', 'application/xml; charset=utf-8');
+      }
+
       response.send(data);
     });
   })
