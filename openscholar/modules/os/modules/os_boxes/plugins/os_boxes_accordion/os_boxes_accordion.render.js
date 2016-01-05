26cc8e9ebafa5c7f572ec5dad02b6941252246ae
digitalpiku <anish.boral@cognizant.com>
2016-01-05 15:11:01 +0530

    fixed accordion widget issue for #7839

--- openscholar/modules/os/modules/os_boxes/plugins/os_boxes_accordion/os_boxes_accordion.render.js
+++ openscholar/modules/os/modules/os_boxes/plugins/os_boxes_accordion/os_boxes_accordion.render.js
@@ -5,6 +5,7 @@
   Drupal.behaviors.osBoxesAccordion = {
     attach: function (ctx) {
       $.each(Drupal.settings.os_boxes.accordion, function (delta, data) {
+        data.delta = delta > 0 ? data.delta + '--' + (parseInt(delta) + 1) : data.delta;
         $('#block-boxes-' + data.delta + ' .accordion', ctx).accordion({
           collapsible: true,
           heightStyle: 'content',
