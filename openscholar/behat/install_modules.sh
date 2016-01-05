be3f8a854dac93147c3bd987bb2c077a9847f8f5
Roy Segall <roy.segall@gizra.com>
2016-01-05 10:19:45 +0200

    Fire up the solr dependencies.

--- openscholar/behat/install_modules.sh
+++ openscholar/behat/install_modules.sh
@@ -6,18 +6,28 @@ fi
 
 
 if [ "${TEST_SUITE}" == "solr" ]; then
-  # Download and install Apache solr
+  # Download and install Apache solr.
   wget https://archive.apache.org/dist/lucene/solr/3.6.2/apache-solr-3.6.2.zip
   unzip -o apache-solr-3.6.2.zip
   cd apache-solr-3.6.2/example/solr/conf
 
-  # Copy the solr config files from the apache solr module
+  # Copy the solr config files from the apache solr module.
   yes | cp /home/travis/build/openscholar/openscholar/www/profiles/openscholar/modules/contrib/apachesolr/solr-conf/solr-3.x/* .
 
-  # Copy the modified solrconfig.xml file to commit records every 20 seconds so items will show up in search sooner
+  # Copy the modified solrconfig.xml file to commit records every 20 seconds so
+  # items will show up in search sooner.
   yes | cp /home/travis/build/openscholar/openscholar/www/profiles/openscholar/behat/solr/solrconfig.xml .
   cd ../../
   java -jar start.jar &
   sleep 10
   cd /home/travis/build/openscholar/openscholar
+
+  # Index site using Apache solr.
+  drush en os_search_solr -y
+  drush solr-mark-all
+  drush solr-index
+
+  # Trying to execute the internal solr commit.
+  sleep 10
+  wget http://localhost:8983/solr/update?commit=true
 fi
