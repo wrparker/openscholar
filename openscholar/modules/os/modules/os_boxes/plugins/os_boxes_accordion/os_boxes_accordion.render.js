a9386dba849501ce8b53b737a63fb5adf5c3952d
digitalpiku <anish.boral@cognizant.com>
2016-01-28 11:17:37 +0530

    jQuery selector altered for #7884

--- openscholar/modules/os/modules/os_boxes/plugins/os_boxes_accordion/os_boxes_accordion.render.js
+++ openscholar/modules/os/modules/os_boxes/plugins/os_boxes_accordion/os_boxes_accordion.render.js
@@ -5,7 +5,7 @@
   Drupal.behaviors.osBoxesAccordion = {
     attach: function (ctx) {
       $.each(Drupal.settings.os_boxes.accordion, function (delta, data) {
-        $('#block-boxes-' + data.delta + ' .accordion:eq(0)', ctx).accordion({
+        $('#boxes-box-' + data.delta + ' > .boxes-box-content > .accordion', ctx).accordion({
           collapsible: true,
           heightStyle: 'content',
           active: data.active
