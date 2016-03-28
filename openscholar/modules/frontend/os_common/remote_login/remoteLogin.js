8624bea333a92f36943a74f94e8501fc5d900ae6
RoySegall <roy.segall@gizra.com>
2016-03-28 11:46:23 +0300

    Display the user name after remove login.

--- openscholar/modules/frontend/os_common/remote_login/remoteLogin.js
+++ openscholar/modules/frontend/os_common/remote_login/remoteLogin.js
@@ -12,14 +12,14 @@ angular.module('remoteLogin', [])
         }
       })
       .success(function(data) {
-        $scope.status = "Hi there! you are logged in!";
+        $scope.status = "Hi there! you are logged in! Fetching your information....";
 
         $http.get(Drupal.settings.os_restful.parentUrl + '/api/users/me', {
           headers: {
             'access-token': data.access_token
           }
         }).success(function(data) {
-          console.log(data)
+          $scope.status = 'OK! you are recognised as ' + data.data[0].label;
         });
       })
       .error(function(data) {
