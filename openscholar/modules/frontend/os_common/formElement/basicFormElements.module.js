693173856f3213ece8a808c9fec93808d1f735f6
Matt Petrovic <mpetrovic@iq.harvard.edu>
2016-04-12 14:18:00 -0400

    #7776: Add text field directive.

--- openscholar/modules/frontend/os_common/formElement/basicFormElements.module.js
+++ openscholar/modules/frontend/os_common/formElement/basicFormElements.module.js
@@ -2,7 +2,13 @@
 
   var m = angular.module('basicFormElements', ['osHelpers']);
 
-  m.directive('checkbox', ['$filter', function ($filter) {
+  /**
+   * Checkbox directive.
+   * Arguments:
+   *   name - string - the name of the element as Drupal expects it
+   *   value - property on parent scope
+   */
+  m.directive('checkbox', [function () {
     return {
       scope: {
         name: '@',
@@ -10,7 +16,22 @@
       },
       template: '<input type="checkbox" id="{{id}}" name="{{name}}" value="1" class="form-checkbox" ng-model="value" ng-true-value="1" ng-false-value="0"/>',
       link: function (scope, elem, attr) {
-        console.log(scope);
+        scope.id = attr['inputId'];
+      }
+    }
+  }]);
+
+  /**
+   * Textbox directive.
+   */
+  m.directive('textfield', [function () {
+    return {
+      scope: {
+        name: '@',
+        value: '=',
+      },
+      template: '<input type="textfield" id="{{id}}" name="{{name}}" ng-model="value" class="form-text">',
+      link: function (scope, elem, attr) {
         scope.id = attr['inputId'];
       }
     }
