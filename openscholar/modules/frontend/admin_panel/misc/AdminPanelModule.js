8ee5a5b80a7923c37012fa42d345d20d811841f9
Dan Cabral <rdancabral@gmail.com>
2016-04-17 12:12:18 -0400

    remove closeEl2 references from js. #8310

--- openscholar/modules/frontend/admin_panel/misc/AdminPanelModule.js
+++ openscholar/modules/frontend/admin_panel/misc/AdminPanelModule.js
@@ -187,7 +187,6 @@
        link: function(scope, element, attrs) {
     	  morphButton = new UIMorphingButton(element[0], {
       			closeEl : '.icon-close',
-      			closeEl2 : '.close-panel',
       			onBeforeOpen : function() {
       				// push main admin_panel
       				jQuery('#page_wrap, .page-cp #page, .page-cp #branding').addClass('pushed');
--- openscholar/modules/frontend/admin_panel/misc/uiMorphingButton_fixed.js
+++ openscholar/modules/frontend/admin_panel/misc/uiMorphingButton_fixed.js
@@ -40,7 +40,6 @@
 
 	UIMorphingButton.prototype.options = {
 		closeEl : '',
-		closeEl2 : '',
 		onBeforeOpen : function() { return false; },
 		onAfterOpen : function() { return false; },
 		onBeforeClose : function() { return false; },
@@ -73,12 +72,7 @@
 				closeEl.addEventListener( 'click', function() { self.toggle(); } );
 			}
 		}
-		if( this.options.closeEl2 !== '' ) {
-			var closeEl2 = this.el.querySelector( this.options.closeEl2 );
-			if( closeEl2 ) {
-				closeEl2.addEventListener( 'click', function() { self.toggle(); } );
-			}
-		}
+		
 	}
 
 	UIMorphingButton.prototype.toggle = function() {
