3fe567fe10bcdfa242bd19607b7d7afa3840f1ed
Ethan Mallove <mallove@fas.harvard.edu>
2016-04-20 14:44:24 -0400

    remove "placeholder" text

--- openscholar/themes/os_basetheme/templates/page.tpl.php
+++ openscholar/themes/os_basetheme/templates/page.tpl.php
@@ -121,7 +121,7 @@
 
 								<?php if ($content = render($page['content'])): ?>
 									<div id="content">
-										<div id="login-announcements" class="login-announcements">Placeholder</div>
+										<div id="login-announcements" class="login-announcements" />
 										<?php print $content; ?>
 									</div>
 								<?php endif; ?>
