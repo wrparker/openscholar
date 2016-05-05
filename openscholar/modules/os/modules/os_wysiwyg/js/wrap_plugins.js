4010326fb5accb5536b98b6f6edd8ed615e18587
RoySegall <roy.segall@gizra.com>
2016-05-05 17:06:17 +0300

    Wrap the justify plugin.

--- openscholar/modules/os/modules/os_wysiwyg/js/wrap_plugins.js
+++ openscholar/modules/os/modules/os_wysiwyg/js/wrap_plugins.js
@@ -1,2 +1,3 @@
 Drupal.wysiwyg.plugins['colorbutton'] = CKEDITOR.plugins.get('colorbutton');
+Drupal.wysiwyg.plugins['justify'] = CKEDITOR.plugins.get('justify');
 Drupal.wysiwyg.plugins['panelbutton'] = CKEDITOR.plugins.get('panelbutton');
