5382225c93c83f2167572d2b9436df36307e1af6
RoySegall <roy.segall@gizra.com>
2016-05-10 16:23:49 +0300

    Using image resize.

--- openscholar/modules/os/modules/os_wysiwyg/js/wrap_plugins.js
+++ openscholar/modules/os/modules/os_wysiwyg/js/wrap_plugins.js
@@ -2,3 +2,4 @@ Drupal.wysiwyg.plugins['colorbutton'] = CKEDITOR.plugins.get('colorbutton');
 Drupal.wysiwyg.plugins['justify'] = CKEDITOR.plugins.get('justify');
 Drupal.wysiwyg.plugins['font'] = CKEDITOR.plugins.get('font');
 Drupal.wysiwyg.plugins['panelbutton'] = CKEDITOR.plugins.get('panelbutton');
+Drupal.wysiwyg.plugins['imageresize'] = CKEDITOR.plugins.get('imageresize');
