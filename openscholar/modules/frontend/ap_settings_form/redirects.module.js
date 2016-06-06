5efde5aefb057da036868776a3e8761ba1308b19
Matt Petrovic <mpetrovic@iq.harvard.edu>
2016-06-06 11:50:53 -0400

    #7776: First pass at template and settings form for redirects.

--- openscholar/modules/frontend/ap_settings_form/redirects.module.js
+++ openscholar/modules/frontend/ap_settings_form/redirects.module.js
@@ -8,12 +8,31 @@
 
   m.directive('redirects', [function() {
     return {
-      template: 'Redirect module activates.',
+      template: '<p>URL redirects allow you to send users from a non-existing path on your site, to any other URL. You might want to use this to create a short link, or to transition users from an old URL that no longer exists to the new URL. Each site may only have a limited number of URL redirects.</p>' +
+      '<ul class="redirect-list">' +
+        '<li class="redirect-item" ng-repeat="r in redirects">' +
+          '<span class="redirect-path">{{r.path}}</span> --> <span class="redirect-target">{{r.target}}</span> <a class="redirect-delete" redirect-id="{{r.id}}">delete</a>' +
+        '</li>' +
+      '</ul>' +
+      '<a class="redirect-add" ng-show="showAddLink()" ng-click="toggleAddForm()">+ Add new redirect</a>' +
+      '<div class="redirect-add-form" ng-show="showAddForm()">' +
+        '<input type="text" ng-model="newRedirectPath"><input type="text" ng-model="newRedirectTarget">' +
+      '</div>',
       scope: {
         value: '=',
         element: '='
       },
-      link: function () {
+      link: function (scope, elem, attr) {
+        var showAddLink = false;
+        scope.showAddLink = function() {
+          return showAddLink;
+        }
+
+        var showAddForm = false;
+        scope.showAddForm = function () {
+          return showAddForm;
+        }
+
 
       }
     };
