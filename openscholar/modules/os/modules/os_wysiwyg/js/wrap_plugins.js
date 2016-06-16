bfd7521e49a498c250d0b11281b5f20f8774ed11
RoySegall <roy.segall@gizra.com>
2016-06-16 11:57:30 +0300

    Adding latex.

--- openscholar/modules/os/modules/os_wysiwyg/js/wrap_plugins.js
+++ openscholar/modules/os/modules/os_wysiwyg/js/wrap_plugins.js
@@ -3,3 +3,6 @@ Drupal.wysiwyg.plugins['justify'] = CKEDITOR.plugins.get('justify');
 Drupal.wysiwyg.plugins['font'] = CKEDITOR.plugins.get('font');
 Drupal.wysiwyg.plugins['panelbutton'] = CKEDITOR.plugins.get('panelbutton');
 Drupal.wysiwyg.plugins['dragresize'] = CKEDITOR.plugins.get('dragresize');
+Drupal.wysiwyg.plugins['mathjax'] = CKEDITOR.plugins.get('mathjax');
+Drupal.wysiwyg.plugins['lineutils'] = CKEDITOR.plugins.get('lineutils');
+Drupal.wysiwyg.plugins['widget'] = CKEDITOR.plugins.get('widget');
