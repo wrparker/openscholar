598e76ff3dfbc75346aa5289ceee23ba904375c0
Ethan Mallove <mallove@fas.harvard.edu>
2016-06-29 09:55:31 -0400

    8562 - Redo "Read More" link changes per @rbran100
    Revert out all of the previous readmore changes thus far in the new branch
    `8562-readmore-redo`, ... the previous solution will cause more bugs in the
    future.
    
    The readmore issue (not showing for profiles) was specific to themes extending
    `hwpi_basetheme`.
    
    We can see what it is doing in `node_view_alter` here:
      https://github.com/openscholar/openscholar/blob/SCHOLAR-3.x/openscholar/themes/hwpi_basetheme/template.php#L194
    
    We noticed that and removed the `unset` on the links which was exactly
    right:
      https://github.com/openscholar/openscholar/pull/8619/files#diff-4773c00a5c8f1dc142ee969856ca0018R257
    
    However, above that (line 207), the `['body']` is completely blown away and moved
    inside of `pic_bio`. This breaks all of our processing of the `body` field for
    readmore links. We have code that only displays the readmore when the body is
    actually long enough but that is not working since `hwpi_basetheme` blows `body`
    away.
    
    So, how to solve this...
    
      1. Getting rid of the `unset` was perfect, redo that
    
      2. We need to have a way of putting the body inside of a `pic_bio` wrapper,
        without removing it. What we need to do, is create that `bio_pic` wrapper and
        move the needed elements inside of it, later in rendering, during
        `hwpi_basetheme_process_node` instead of `hwpi_basetheme_preprocess_node`. Both
        functions already exist, it should just be a matter of moving the wrapper, and
        making sure things get rendered correctly. We can compare the markup before
        and after and it should be the same.... except now the read-more links will
        show.

--- openscholar/themes/hwpi_basetheme/template.php
+++ openscholar/themes/hwpi_basetheme/template.php
@@ -152,46 +152,23 @@ function hwpi_basetheme_profile_default_image($size = 'small') {
 /**
  * Process variables for comment.tpl.php
  */
-function hwpi_basetheme_process_node(&$vars) {
+function hwpi_basetheme_process_node(&$build) {
   // Event persons, change title markup to h1.
-  if ($vars['type'] == 'person') {
-    if ($vars['view_mode'] == 'title') {
-      $vars['title_prefix']['#suffix'] = '<h1 class="node-title">' . l($vars['title'], 'node/' . $vars['nid']) . '</h1>';
-      $vars['title'] = NULL;
+  if ($build['type'] == 'person') {
+    if ($build['view_mode'] == 'title') {
+      $build['title_prefix']['#suffix'] = '<h1 class="node-title">' . l($build['title'], 'node/' . $build['nid']) . '</h1>';
+      $build['title'] = NULL;
     }
-    elseif (!$vars['teaser'] && $vars['view_mode'] != 'sidebar_teaser') {
-      $vars['title_prefix']['#suffix'] = '<h1 class="node-title">' . $vars['title'] . '</h1>';
-      $vars['title'] = NULL;
+    elseif (!$build['teaser'] && $build['view_mode'] != 'sidebar_teaser') {
+      $build['title_prefix']['#suffix'] = '<h1 class="node-title">' . $build['title'] . '</h1>';
+      $build['title'] = NULL;
 
-      if ($vars['view_mode'] == 'slide_teaser') {
-        $vars['title_prefix']['#suffix'] = '<div class="toggle">' . $vars['title_prefix']['#suffix'] . '</div>';
+      if ($build['view_mode'] == 'slide_teaser') {
+        $build['title_prefix']['#suffix'] = '<div class="toggle">' . $build['title_prefix']['#suffix'] . '</div>';
       }
     }
   }
-}
 
-/**
- * Implements hook_field_display_ENTITY_TYPE_alter().
- */
-function hwpi_basetheme_field_display_node_alter(&$display, $context) {
-  if ($context['entity']->type == 'event' && $context['instance']['field_name'] == 'field_date') {
-    if (($context['view_mode'] != 'full') || (isset($context['entity']->os_sv_list_box) && $context['entity']->os_sv_list_box)) {
-
-      if (isset($context['entity']->field_date[LANGUAGE_NONE][0]['value2']) &&
-          (strtotime($context['entity']->field_date[LANGUAGE_NONE][0]['value2']) - strtotime($context['entity']->field_date[LANGUAGE_NONE][0]['value']) > 24*60*60)) {
-        return; //event is more than one day long - keep both dates visible
-      }
-
-      //hide the date - it's already visible in the shield
-      $display['settings']['format_type'] = 'os_time';
-    }
-  }
-}
-
-/**
- * Alter the results of node_view().
- */
-function hwpi_basetheme_node_view_alter(&$build) {
   // Persons, heavily modify the output to match the HC designs
   if ($build['#node']->type == 'person') {
 
@@ -385,8 +362,27 @@ function hwpi_basetheme_node_view_alter(&$build) {
       unset($build['og_vocabulary']);
     }
   }
+
+
 }
 
+/**
+ * Implements hook_field_display_ENTITY_TYPE_alter().
+ */
+function hwpi_basetheme_field_display_node_alter(&$display, $context) {
+  if ($context['entity']->type == 'event' && $context['instance']['field_name'] == 'field_date') {
+    if (($context['view_mode'] != 'full') || (isset($context['entity']->os_sv_list_box) && $context['entity']->os_sv_list_box)) {
+
+      if (isset($context['entity']->field_date[LANGUAGE_NONE][0]['value2']) &&
+          (strtotime($context['entity']->field_date[LANGUAGE_NONE][0]['value2']) - strtotime($context['entity']->field_date[LANGUAGE_NONE][0]['value']) > 24*60*60)) {
+        return; //event is more than one day long - keep both dates visible
+      }
+
+      //hide the date - it's already visible in the shield
+      $display['settings']['format_type'] = 'os_time';
+    }
+  }
+}
 
 /**
  * Preprocess variables for comment.tpl.php
