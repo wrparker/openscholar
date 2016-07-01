0b6e70ac5b9d761560d4dbbfdcd3d7b155ad8b4b
Subhramalya Bose <Subhramalya.Bose@cognizant.com>
2016-07-01 16:12:11 +0530

    Revert first accordion panel close for #8547

--- openscholar/modules/os/modules/os_boxes/plugins/os_boxes_facetapi_vocabulary/os_boxes_facetapi_vocabulary_accordion.render.js
+++ openscholar/modules/os/modules/os_boxes/plugins/os_boxes_facetapi_vocabulary/os_boxes_facetapi_vocabulary_accordion.render.js
@@ -10,7 +10,6 @@
           heightStyle: 'content',
           active: true
         });
-        $('#boxes-box-' + data.delta + ' > .boxes-box-content > .accordion > .item-list:first > h3').trigger('click');
       });
       $('.block-boxes-os_boxes_facetapi_vocabulary a.facetapi-active').closest('div.item-list').find('h3').not('.ui-accordion-header-active').trigger('click');
     }
