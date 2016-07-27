3344a3335f43fcdcfb96453b683eb2d29c3375de
Dan Cabral <rdancabral@gmail.com>
2016-07-27 14:12:43 -0400

    tweak session expiry message. #8674

--- openscholar/modules/os/js/os_session_timeout.js
+++ openscholar/modules/os/js/os_session_timeout.js
@@ -43,7 +43,7 @@
       dataType:'json',
       success: function(jData) {
         if (jData.show_warning == 1) {
-          jQuery('#page').prepend('<div id="timeout-warning-wrapper" class="messages warning element-hidden"><div class="message-inner"><div class="message-wrapper">Warning: Your logged-in session will expire in <span id="session-timeout-timer" data-rooster-seconds=' + Drupal.settings.os.warning_interval_before_timeout + '></span> minutes. <a href="javascript:extend_os_session();" class="session-extend-link">Extend your session here.</div></div></div>');
+          jQuery('#page').prepend('<div id="timeout-warning-wrapper" class="messages warning element-hidden"><div class="message-inner"><div class="message-wrapper">Warning: Your session will expire in <span id="session-timeout-timer" data-rooster-seconds=' + Drupal.settings.os.warning_interval_before_timeout + '></span> minutes. <a href="javascript:extend_os_session();" class="session-extend-link">Click here to continue your session.</div></div></div>');
           jQuery('#timeout-warning-wrapper').slideDown('slow');
           jQuery('#session-timeout-timer').rooster('start');
         }
