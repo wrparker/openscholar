c46c0c65533d64d53240ca487892371112a392be
Dan Cabral <rdancabral@gmail.com>
2016-08-30 16:47:45 -0400

    tweak pager styles. swap out arrows. #8779

--- openscholar/themes/os_basetheme/template.php
+++ openscholar/themes/os_basetheme/template.php
@@ -602,7 +602,7 @@ function os_basetheme_views_mini_pager($vars) {
 
     $li_previous = theme('pager_previous',
       array(
-        'text' => (isset($tags[1]) ? $tags[1] : t('‹')),
+        'text' => (isset($tags[1]) ? $tags[1] : t('&#65513;')),
         'element' => $element,
         'interval' => 1,
         'parameters' => $parameters,
@@ -614,7 +614,7 @@ function os_basetheme_views_mini_pager($vars) {
 
     $li_next = theme('pager_next',
       array(
-        'text' => (isset($tags[3]) ? $tags[3] : t('›')),
+        'text' => (isset($tags[3]) ? $tags[3] : t('&#65515;')),
         'element' => $element,
         'interval' => 1,
         'parameters' => $parameters,
