13f66e4adfddae2ee0b87860d8ddec34a52a39b3
Dan Cabral <rdancabral@gmail.com>
2016-08-31 15:40:14 -0400

    tweak arrows again. #8779

--- openscholar/themes/os_basetheme/template.php
+++ openscholar/themes/os_basetheme/template.php
@@ -602,7 +602,7 @@ function os_basetheme_views_mini_pager($vars) {
 
     $li_previous = theme('pager_previous',
       array(
-        'text' => (isset($tags[1]) ? $tags[1] : t('◀')),
+        'text' => (isset($tags[1]) ? $tags[1] : t('<<')),
         'element' => $element,
         'interval' => 1,
         'parameters' => $parameters,
@@ -614,7 +614,7 @@ function os_basetheme_views_mini_pager($vars) {
 
     $li_next = theme('pager_next',
       array(
-        'text' => (isset($tags[3]) ? $tags[3] : t('▶')),
+        'text' => (isset($tags[3]) ? $tags[3] : t('>>')),
         'element' => $element,
         'interval' => 1,
         'parameters' => $parameters,
