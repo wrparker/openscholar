08af7b25795ad04b69e4c7c150cacf506e022143
Rebecca Asch <rsasch@speakeasy.net>
2016-09-01 16:56:43 -0400

    added javascript confirm for when user reaches max number of revisions

---
+++ openscholar/modules/os/modules/os_revisions/os_revisions.js
@@ -0,0 +1,23 @@
+(function ($) {
+  /**
+   * Display warning message when user goes to create new revision when node already has
+   * the max number for that content type
+   */
+  Drupal.behaviors.osRepeatingEventChange = {
+    attach: function () {
+      if ($("input[name='max_revisions']").val()) {
+          $("input#edit-revision").click(function () {
+              $max = $("input[name='max_revisions']").val();
+              $current = $("input[name='revisions']").val();
+              if ($max && $current && $(this).attr('checked')) {
+                  if ($current > $max) {
+                      if (!confirm('This content already has the maximum number of revisions saved (' + $max + '). By creating a new revision, you will be permanently deleting the oldest revision.\n\nAre you sure you want to create a new revision?')) {
+                          $(this).removeAttr('checked');
+                      }
+                  }
+              }
+          });
+      }
+    }
+  }
+})(jQuery);
