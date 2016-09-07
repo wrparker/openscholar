4a648c26e0632db9dd3f15bafe9c42b263993e4f
digitalpiku <anish.boral@cognizant.com>
2016-09-07 13:15:22 +0530

    Fixed iframe width issue for #7909

--- openscholar/modules/os/modules/os_boxes/misc/os_boxes.resize_iframe.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-  jQuery.each(Drupal.settings.widget_max_width, function(wrapper_id, width){
-    var iframe = jQuery('#' + wrapper_id + ' iframe');
-    jQuery(iframe).attr('width', width); 
-    jQuery(iframe).attr('scrolling', 'auto'); 
-  });
-});
--- openscholar/modules/os/modules/os_boxes/misc/os_boxes.resize_parent.js
+++ openscholar/modules/os/modules/os_boxes/misc/os_boxes.resize_parent.js
@@ -18,9 +18,10 @@
     var iframes = document.querySelectorAll('iframe[src="'+data.url+'"]');
 
     for (i=0; i<iframes.length; i++) {
-      if (typeof Drupal.settings.widget_max_width[i] != 'undefined' && Drupal.settings.widget_max_width[i] != '') {        
+      var delta = jQuery(iframes[i]).closest('.boxes-box').attr('id');
+      if (typeof Drupal.settings.widget_max_width != 'undefined' && typeof Drupal.settings.widget_max_width[delta] != 'undefined' && Drupal.settings.widget_max_width[delta] != '' && data.width > Drupal.settings.widget_max_width[delta]) {
         if (!iframes[i].resized) {
-          iframes[i].width = Drupal.settings.widget_max_width[i];
+          iframes[i].width = Drupal.settings.widget_max_width[delta];
           jQuery(iframes[i]).attr("scrolling", "auto");
           jQuery(iframes[i]).attr("src", jQuery(iframes[i]).attr("src"));
           iframes[i].resized = true;
