9dea617439f583778f8924d778d9af78e48b0e04
Matt Petrovic <mpetrovic@iq.harvard.edu>
2016-09-15 14:00:36 -0400

    #7776: Add module to print messages to screen after response.

---
+++ openscholar/modules/frontend/restful_messages/messages.module.js
@@ -0,0 +1,29 @@
+(function () {
+  var m = angular.module('Messages', ['ngStorage']);
+
+  m.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('messageFetcher')
+  }]);
+
+  m.factory('messageFetcher', ['$sessionStorage', function ($ss) {
+    var messageFetcher = {
+      response: function (response) {
+        if (response.data && response.data.messages) {
+          $ss.messages = response.data.messages;
+        }
+        return response;
+      }
+    }
+
+    return messageFetcher;
+  }]);
+
+  m.controller('messageController', ['$scope', '$sessionStorage', function ($s, $ss) {
+    $s.messages = $ss.messages;
+    console.log($s.messages);
+
+    this.close = function () {
+      delete $s.messages;
+    }
+  }]);
+})()
