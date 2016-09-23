e2b782b041d5ebe5b16fe49aaefb3cb9c1a6b2c3
Rebecca Asch <rsasch@speakeasy.net>
2016-09-23 13:04:04 -0400

    modified text in revision information fieldset; added text to revisions list; changed title/body of revert/delete confirm modals

--- openscholar/modules/os/modules/os_revisions/os_revisions.js
+++ openscholar/modules/os/modules/os_revisions/os_revisions.js
@@ -1,23 +1,32 @@
 (function ($) {
-  /**
-   * Display warning message when user goes to create new revision when node already has
-   * the max number for that content type
-   */
-  Drupal.behaviors.osRepeatingEventChange = {
+  Drupal.behaviors.osRevisionsConfirms = {
     attach: function () {
+      /**
+       * Display warning message when user goes to create new revision when node already has
+       * the max number for that content type
+       */
       if ($("input[name='max_revisions']").val()) {
-          $max = $("input[name='max_revisions']").val();
-          $current = $("input[name='revisions']").val();
-          $("input#edit-revision").click(function () {
-              if ($max && $current && $(this).prop('checked')) {
-                  if ($current > $max) {
-                      if (!confirm('This content already has the maximum number of revisions saved (' + $max + '). By creating a new revision, you will be permanently deleting the oldest revision.\n\nAre you sure you want to create a new revision?')) {
-                          $(this).removeAttr('checked');
-                      }
-                  }
+        $max = $("input[name='max_revisions']").val();
+        $current = $("input[name='revisions']").val();
+        $("input#edit-revision").click(function () {
+          if ($max && $current && $(this).prop('checked')) {
+            if ($current > $max) {
+              if (!confirm('This content already has the maximum number of revisions saved (' + $max + '). By creating a new revision, you will be permanently deleting the oldest revision.\n\nAre you sure you want to create a new revision?')) {
+                $(this).removeAttr('checked');
               }
-          });
+            }
+          }
+        });
       }
+      /**
+       * Display warning message when user clicks link to view revisions before any changes
+       * that have been made have been saved
+       */
+      $("fieldset#edit-revision-information a#revisions-link").click(function(e) {
+        if (!confirm('Leaving this page will leave your changes unsaved.')) {
+          e.preventDefault();
+        }
+      });
     }
   }
 })(jQuery);
