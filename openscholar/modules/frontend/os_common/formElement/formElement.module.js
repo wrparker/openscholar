43b15d8532beb9daa00380abc70f4a72b59364e1
Matt Petrovic <mpetrovic@iq.harvard.edu>
2016-10-28 14:32:58 -0400

    #7776: Replace 'value' attribute with 'ng-model'

--- openscholar/modules/frontend/os_common/formElement/formElement.module.js
+++ openscholar/modules/frontend/os_common/formElement/formElement.module.js
@@ -49,7 +49,7 @@
           copy.attr('element', 'element');
 
           copy.attr('input-id', scope.id);
-          copy.attr('value', 'value');
+          copy.attr('ng-model', 'value');
           elem.find('span').replaceWith(copy);
           copy = $compile(copy)(scope);
           if (scope.element.attached) {
