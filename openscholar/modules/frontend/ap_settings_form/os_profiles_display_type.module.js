8aa655e7d2fafc0d1459e2edd234d6a62604d11e
RoySegall <roy.segall@gizra.com>
2016-11-21 10:29:04 +0200

    Adding switcher for the display types.

--- openscholar/modules/frontend/ap_settings_form/os_profiles_display_type.module.js
+++ openscholar/modules/frontend/ap_settings_form/os_profiles_display_type.module.js
@@ -14,12 +14,12 @@
           '<div class="first">' +
             '<div class="form-radios">' +
               '<div class="form-item form-type-radio" ng-repeat="(option, label) in options">' +
-                '<input type="radio" name="os_profiles_display_type" value="{{option}}" /> {{label}}' +
+                '<input type="radio" ng-click="changeDisplay(option)" name="os_profiles_display_type" value="{{option}}" ng-checked="{{option == default_value}}" /> {{label}}' +
               '</div>' +
             '</div>' +
           '</div>' +
           '<div class="second">' +
-            '<p ng-bind-html="preview"></p>' +
+            '<p ng-bind-html="preview" class="preview"></p>' +
           '</div>' +
         '</div>',
 
@@ -32,7 +32,12 @@
         scope.title = scope.element.title;
         scope.previews = scope.element.previews;
         scope.options = scope.element.options;
+        scope.default_value = scope.element.value;
         scope.preview = scope.previews[scope.element.value];
+
+        scope.changeDisplay = function(option) {
+          scope.preview = scope.previews[option];
+        };
       }
     };
   }]);
