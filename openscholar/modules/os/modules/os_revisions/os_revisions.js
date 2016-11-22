2ce43daba24a1f25dfcf0c583852f4375ef4d1fe
Rebecca Asch <rsasch@speakeasy.net>
2016-11-22 13:29:15 -0500

    changed revisions to be created by default; revision fieldset is un-collapsed to show warning if user has more than max # of revisions for post

--- openscholar/modules/os/modules/os_revisions/os_revisions.js
+++ openscholar/modules/os/modules/os_revisions/os_revisions.js
@@ -2,22 +2,15 @@
   Drupal.behaviors.osRevisionsConfirms = {
     attach: function () {
       /**
-       * Display warning message when user goes to create new revision when node already has
+       * Un-collapse the revision fieldset when node already has
        * the max number for that content type
        */
       if ($("input[name='max_revisions']").val()) {
         $max = $("input[name='max_revisions']").val();
         $current = $("input[name='revisions']").val();
-        $("input#edit-revision").click(function () {
-          if ($max && $current && $(this).prop('checked')) {
-            if ($current - $max >= 0) {
-              $('#warning-text').html('<p>Are you sure you want to create a new revision?</p><p>This content already has the maximum number of revisions saved (' + $max + '). By creating a new revision, you will be permanently deleting the oldest revision. Learn how to manage your revisions on our <a href="#" target="_blank" rel="noopener"> documentation site</a>.</p>').show();
-            }
-          }
-          else {
-            $('#warning-text').hide();
-          }
-        });
+        if ($current - $max >= 0) {
+          $('fieldset#edit-revision-information').removeClass('collapsed');
+        }
       }
       /**
        * Display warning message when user clicks link to view revisions before any changes
