5047354281691178157c5935eebbb246c5a34d10
Matt Petrovic <mpetrovic@iq.harvard.edu>
2016-12-07 14:41:51 -0500

    #9024: Pare this down to the bare essentials.

--- openscholar/modules/frontend/ap_settings_form/os_profiles_display_type.module.js
+++ openscholar/modules/frontend/ap_settings_form/os_profiles_display_type.module.js
@@ -6,39 +6,23 @@
     $depProvider.AddDependency('formElement', 'displayTypeForm');
   }]);
 
-  m.directive('displayType', ['$http', 'buttonSpinnerStatus', function($http, bss) {
+  m.directive('displayType', ['$timeout', function($t) {
     return {
-      template:
-        '<div class="profiles-display-types clearfix">' +
-          '<label for="edit-os-profiles-disable-default-image">{{title}}</label>' +
-          '<div class="first">' +
-            '<div class="form-radios">' +
-              '<div class="form-item form-type-radio" ng-repeat="(option, label) in options">' +
-                '<input type="radio" ng-click="changeDisplay(option)" name="os_profiles_display_type" value="{{option}}" ng-checked="{{option == default_value}}" ng-model="value" /> {{label}}' +
-              '</div>' +
-            '</div>' +
-          '</div>' +
-          '<div class="second">' +
-            '<p ng-bind-html="preview" class="preview"></p>' +
-          '</div>' +
-        '</div>',
-
-      scope: {
-        value: '=',
-        element: '='
-      },
-
+      priority: 10,
       link: function (scope, elem, attr) {
-        scope.title = scope.element.title;
-        scope.previews = scope.element.previews;
-        scope.options = scope.element.options;
-        scope.default_value = scope.element.value;
-        scope.preview = scope.previews[scope.element.value];
+        function changeDisplay (e) {
+          var key = e.currentTarget.value;
+          wrapper.html(scope.element.previews[key]);
+        }
 
-        scope.changeDisplay = function(option) {
-          scope.preview = scope.previews[option];
-        };
+        // This needs to be in a $timeout because the input elements haven't been cloned by this point.
+        $t(function () {
+          var inputs = elem.find('input');
+          inputs.bind('click', changeDisplay);
+        });
 
+        var wrapper = angular.element('<div id="os-profiles-preview"></div>');
+        elem.append(wrapper);
       }
     };
   }]);
