4c45cf8d6f540938221fb7a6f5052911c84d148e
Dan Cabral <rdancabral@gmail.com>
2016-12-15 10:55:04 -0500

    js tweak. #8496

--- openscholar/themes/custom/garyking/garyking.js
+++ openscholar/themes/custom/garyking/garyking.js
@@ -54,7 +54,7 @@
  jQuery(document).ready(function() {
   if (jQuery('#biblio-node').find('div.biblio-upload-wrapper').length) {
     jQuery('#citation-wrapper').addClass('has-attached');
- 
+ }
  
  var setHeight = jQuery(".has-attached");
 jQuery(".biblio-upload-wrapper").css({height:setHeight.height()});
