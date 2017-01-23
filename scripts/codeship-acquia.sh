22afd099bfaaf3442fa7c4f5670cde2c354f9254
Matt Petrovic <mpetrovic@iq.harvard.edu>
2017-01-23 15:52:23 -0500

    oy

--- scripts/codeship-acquia.sh
+++ scripts/codeship-acquia.sh
@@ -39,7 +39,7 @@ cp -f openscholar/openscholar/bower.json /tmp/
 echo "Beginning subtree pull."
 #git subtree pull -d --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH
 rm -rf openscholar/openscholar
-cp -f ~/src/github.com/openscholar/openscholar /home/rof/acquia.com/hwpi1/openscholar
+cp -rf ~/src/github.com/openscholar/openscholar /home/rof/acquia.com/hwpi1/openscholar
 echo "Subtree pull finished."
 #Only build if no build has ever happened, or if the make files have changed
 if [ ! -d openscholar/openscholar/modules/contrib ] || [ "$(cmp -b 'openscholar/openscholar/drupal-org-core.make' '/tmp/drupal-org-core.make')" != "" ] || [ "$(cmp -b 'openscholar/openscholar/drupal-org.make' '/tmp/drupal-org.make')" != "" ] || [ "$(cmp -b 'openscholar/openscholar/client/bower.json' '/tmp/bower.json')" != "" ]; then
