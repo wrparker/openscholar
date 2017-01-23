4084d4188ade40a9c3a2cab42519ce9492344cb4
Matt Petrovic <mpetrovic@iq.harvard.edu>
2017-01-23 14:24:21 -0500

    Pear upgrade so drush pear channel works.

--- scripts/codeship-acquia.sh
+++ scripts/codeship-acquia.sh
@@ -19,6 +19,7 @@ if ! test "$PR_BRANCH" = ""; then
 git push origin :$PR_BRANCH || echo "$PR_BRANCH not found on hwpi1"
 fi
 # Build this branch and push it to Acquia
+pear upgrade
 pear channel-discover pear.drush.org
 pear install drush/drush-5.9.0
 mkdir ~/.drush
