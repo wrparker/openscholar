45bc6cd6e9f3c80ff003018441702d2ac43a954c
Matt Petrovic <mpetrovic@iq.harvard.edu>
2017-02-28 14:38:12 -0500

    #8922: Modal opens. Looks alright for now. Tabs work.

--- openscholar/modules/os/modules/os_wysiwyg_link/js/OsWysiwygLinkTool.module.js
+++ openscholar/modules/os/modules/os_wysiwyg_link/js/OsWysiwygLinkTool.module.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var m = angular.module('OsWysiwygLinkTool', ['EntityService', 'JSPager', 'angularModalService']);
+  var m = angular.module('OsWysiwygLinkTool', ['EntityService', 'JSPager', 'angularModalService', 'ui.bootstrap']);
 
   m.service('OWLModal', ['ModalService', function (ModalService) {
     var dialogParams = {
@@ -27,49 +27,7 @@
     return {
       open: function (text, type, urlArgument, titleText, newWindow, close) {
         ModalService.showModal({
-          template: '<div>' +
-          '<div class="form-item">' +
-            '<label for="owl-field-text"><input type="text" id="owl-field-text" ng-model="text">' +
-            '<div class="description">The text of the link. Leave blank to use the url of the link itself.</div>' +
-          '</div>' +
-          '<div class="form-item">' +
-            '<label for="owl-title-text">Title Text</label><input type="text" id="owl-title-text" ng-model="title">' +
-            '<div class="description">The text will appear on mouse hover, and is used by screen readers. It will not appear for keyboard or touch-only users.</div>' +
-          '</div>' +
-          '<div class="form-item">' +
-            '<input type="checkbox" id="owl-target" ng-model="newWindow"><label for="owl-target">Open this link in a new tab</label>' +
-            '<div class="description">Note: depending on usage, may cause accessibility concenrs. <a href="https://www.w3.org/TR/WCAG20-TECHS/G200.html" target="_blank">Learn more</a></div>' +
-          '</div>' +
-          '<uib-tabset active="active">' +
-            '<uib-tab index="href" id="href-link">' +
-              '<div class="form-item">' +
-                '<label for="owl-url">Website URL</label><input type="text" id="owl-url" ng-model="url">' +
-                '<div class="description">The URL of the web page. Can be a link to one of your own pages.</div>' +
-              '</div>' +
-            '</uib-tab>' +
-            '<uib-tab index="email" id="email-link">' +
-              '<label for="owl-email">E-mail</label><input type="text" id="owl-email" ng-model="arg">' +
-            '</uib-tab>' +
-            '<uib-tab index="file" id="file-link">' +
-              '<div class="owl-file-controls">'+
-                '<div class="add_new">Add New' +
-                  '<div class="form-help">?</div>' +
-                  '<div class="description">Files must be less than <strong>{{filesize}}</strong>.<br />Allowed file types: {{extensionStr}}</div>' +
-                '</div>' +
-                'Or' +
-                '<div class="search"></div>' +
-              '</div>' +
-              '<div class="owl-file-list">' +
-                '<div js-pager="f in files" class="owl-file-item" ng-class="{selected: f.id == arg}" ng-click="arg = f.id">{{f.name}}' +
-                '</div>' +
-              '</div>' +
-            '</uib-tab>' +
-          '</uib-tabset>' +
-          '<div class="form-actions">' +
-            '<button ng-click="close(true)">Insert</button>' +
-            '<button ng-click="close(false)">Close</button>' +
-          '</div>' +
-          '</div>',
+          templateUrl: Drupal.settings.paths.owl + '/theme/OsWysiwygLinkTool.template.html',
           controller: 'OWLModalController',
           inputs: {
             params: {
@@ -100,7 +58,7 @@
     $s.arg = params.arg;
     $s.title = params.title;
     $s.newWindow = params.newWindow;
-    $s.active = params.type;
+    $s.active = params.type || 'url';
 
     var params = {
       schema: 'public'
