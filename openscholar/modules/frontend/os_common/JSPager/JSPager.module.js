8051fa553903eac2b411f4cb2e4854757b67284f
Matt Petrovic <mpetrovic@iq.harvard.edu>
2017-03-01 14:02:20 -0500

    #8922: Support automatically paging to the page with a given item on it.

--- openscholar/modules/frontend/os_common/JSPager/JSPager.module.js
+++ openscholar/modules/frontend/os_common/JSPager/JSPager.module.js
@@ -45,7 +45,7 @@
       }
       return currentPage;
     })
-    .directive('jsPager', ['$parse', function($parse) {
+    .directive('jsPager', ['$parse', '$filter', function($parse, $filter) {
       var currentPages = [],
         idMap = {};
       return {
@@ -73,13 +73,14 @@
         compile: function pagerCompile(element, attr) {
           var loop = attr.jsPager,
           /* this regex matches the following patterns:
-           * var in collection | filter:item1 | filter:item2 track by var.property
+           * var in collection | filter:item1 | filter:item2 track by var.property starting with argument
            * if you try to do 'var in collection track by var.property | filters' it won't work properly
            */
-            match = attr.jsPager.match(/^\s*([\s\S]+?)\s+in\s+([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+track\s+by\s+([\s\S]+?))?\s*$/),
+            match = attr.jsPager.match(/^\s*([\s\S]+?)\s+in\s+([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+track\s+by\s+([\s\S]+?)(?:\s+starting\s+with\s+([\s\S]+?))?)?\s*$/),
             index = match[1],
             collection = match[2],
             trackExp = match[4],
+            starting = match[5],
             elements = [],
             pageSize = attr.pageSize || 10,
             //allElements = {$id: hashKey},
@@ -116,6 +117,31 @@
             }
 
             pagerLink($scope, $element, $attr, _c);
+
+            // Determine the starting page
+            if (starting) {
+              var page = 0,
+                pagerCopy = angular.copy($scope.pager),
+                dataset = $parse(collection)($scope.$parent),
+                indexKey = trackExp.replace(index+'.' , ''),
+                argValue = $parse(starting)($scope.$parent);
+
+              if (argValue) {
+                var found = false;
+                do {
+                  pagerCopy.setPage(page++);
+                  var results = $filter('PagerCurrentPage')(dataset, pagerCopy)
+                  for (var i = 0; i < results.length; i++) {
+                    found = found || (results[indexKey] == argValue);
+                  }
+                } while (!found && pagerCopy.currentPage() < pagerCopy.numPages());
+
+                if (found) {
+                  $scope.pager.setPage(pagerCopy.currentPage());
+                }
+              }
+            }
+
             $scope.$watchCollection(collection, function(collection) {
               $scope.collectionLength = 0;
               if (Array.isArray(collection)) {
@@ -174,6 +200,7 @@
       numPages: numPages,
       canPage: canPage,
       changePage: changePage,
+      setPage: setPage,
       pageSize: parseInt(iAttrs.pageSize) || 10
     };
 
@@ -204,6 +231,12 @@
         dir = parseInt(dir);
         current += dir;
         controller.page(JSPagerId, current);
+      }
+    }
+
+    function setPage(num) {
+      if (0 <= num && num < numPages()) {
+        curent = num;
       }
     }
   }
