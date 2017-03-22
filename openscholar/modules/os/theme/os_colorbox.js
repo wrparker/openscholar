34517d25a198bc6a5326abebefd25111edbf87cd
Subhramalya Bose <smboser@gmail.com>
2017-03-22 18:23:29 +0530

    Preloading colorbox images #9021

--- openscholar/modules/os/theme/os_colorbox.js
+++ openscholar/modules/os/theme/os_colorbox.js
@@ -8,3 +8,22 @@ Drupal.behaviors.osColorbox = {
     });
   }
 };
+
+jQuery(document).ready(function(){
+  jQuery("a.media-gallery-thumb").each(function() {
+    var largeUrl = jQuery(this).attr('href');
+    var $t = jQuery(this);
+    jQuery.ajax({
+      url: largeUrl,
+      success: function(data) {
+        var regex = /src="(.*?)"/;
+        var srcArray = regex.exec(data.toString());
+        if (typeof srcArray != 'undefined') {
+          var src = srcArray[1];
+          $t.append('<div class="hideclass"><img src="' + src + '" /></div>');
+          $t.children('.hideclass').hide();
+        }
+      },
+    });
+  });
+});
