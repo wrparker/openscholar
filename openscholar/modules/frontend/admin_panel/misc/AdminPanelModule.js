f8ccf7a6161dc8fa0651b12e51cda590a028839d
Matt Petrovic <mpetrovic@iq.harvard.edu>
2017-04-14 11:51:16 -0400

    #9036: Add dependency.

--- openscholar/modules/frontend/admin_panel/misc/AdminPanelModule.js
+++ openscholar/modules/frontend/admin_panel/misc/AdminPanelModule.js
@@ -5,7 +5,7 @@
   var uid;
   var morphButton;
 
-    angular.module('AdminPanel', [ 'os-auth', 'ngCookies','ngStorage', 'RecursionHelper', 'ApSettingsForm'])
+    angular.module('AdminPanel', [ 'os-auth', 'ngCookies','ngStorage', 'RecursionHelper', 'ApSettingsForm', 'AppForm'])
     .config(function (){
        paths = Drupal.settings.paths
        vsite = typeof Drupal.settings.spaces != 'undefined' ? Drupal.settings.spaces.id : 0;
