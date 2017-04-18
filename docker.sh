862662af3fde1742370fea7e8c242061ae2edc23
RoySegall <roy.segall@gizra.com>
2017-04-18 15:34:41 +0300

    Document the workflow with docker.

--- docker.sh
+++ docker.sh
@@ -3,4 +3,15 @@
 set -e
 
 docker build -t openscholar .
-docker run -it -p 8080:80 -e TEST_SUITE=$TEST_SUITE openscholar
+docker run -it -p 8080:80 -e TEST_SUITE=restful openscholar bash
+
+# After the docker will finish the command above it will terminate it self.
+# In order to keep docker up and running you need to:
+# 1. Run docker ps -a -q
+# 2. Run docker start HASH
+#
+#
+# Docker will not delete the living containers. Docker containers takes space on
+# your HDD. In order to remove them type:
+# docker stop $(docker ps -a -q)
+# docker rm $(docker ps -a -q)
