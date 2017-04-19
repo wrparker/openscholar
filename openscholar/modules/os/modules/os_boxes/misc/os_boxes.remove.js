f5fe0523b44bbede7962e9be0ded90a29a1ec6e4
Asmi94 <Asmita.Bhattacharyya@cognizant.com>
2017-04-19 19:48:16 +0530

    Test 66

--- openscholar/modules/os/modules/os_boxes/misc/os_boxes.remove.js
+++ openscholar/modules/os/modules/os_boxes/misc/os_boxes.remove.js
@@ -1,14 +1,15 @@
 
 (function ($) {
 
-  /*$(document).ready(function(){
-
+  Drupal.behaviors.osBase_appendSpanIframe = {
+  attach : function(ctx) {
     if($('article.node').find("iframe").length){
       var floatValue = $('article.node').find("iframe").parents(".media-element-container").css('float');
-      $('article.node').find("iframe").wrap("<span></span>");
-      $('article.node').find("iframe").parent().css({"float" : floatValue});
+            $('article.node').find("iframe").wrap("<span></span>");
+            $('article.node').find("iframe").parent().css({"float" : floatValue});
+        }
     }
-  });*/
+};
 
 Drupal.behaviors.osBoxesRemove = {
   attach: function (ctx) {
