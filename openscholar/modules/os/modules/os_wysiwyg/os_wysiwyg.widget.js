96c4e9321b6c9ae9574bd8e0f8c44d6c1f29e80f
RoySegall <roy.segall@gizra.com>
2017-05-17 11:56:43 +0300

    Remove any font attribute from the pasted text.

--- openscholar/modules/os/modules/os_wysiwyg/os_wysiwyg.widget.js
+++ openscholar/modules/os/modules/os_wysiwyg/os_wysiwyg.widget.js
@@ -24,13 +24,62 @@
 
         instance.find('#cke_1_contents').height(editor_height);
 
-        editor.on('doubleclick', function(evt)  {
-          if (evt.data.element.is('a')) {
-            evt.data.dialog = null;
+        editor.on('doubleclick', function(event)  {
+          if (event.data.element.is('a')) {
+            event.data.dialog = null;
           }
         }, null, null, 100);
 
-        editor.on('selectionChange', function(evt) {
+        editor.on('paste', function (event) {
+          // Create a dummy element from scratch.
+          var div = document.createElement('div');
+          // Assign the pasted value as the text. Ths will help us search the font-*
+          // attributes we want to remove.
+          div.innerHTML = event.data.dataValue;
+
+          var allTags = div.getElementsByTagName("*");
+
+          // Start iterate over all the tags.
+          // We are iterating over the tags and just find and replace using
+          // regex because we might have html inside the body as a code example.
+          for (var i = 0, len = allTags.length; i < len; i++) {
+            var tag = allTags[i];
+
+            for (var attributes_i = 0; attributes_i < tag.attributes.length; attributes_i++) {
+              if (tag.attributes[attributes_i].nodeName != 'style') {
+                // This is not a style attribute. Skipping.
+                continue;
+              }
+
+              // Take the value of the style attributes and tare them apart.
+              var attribute_values = tag.attributes[attributes_i].value.split(" ");
+              for (var attribute_values_i = 0; attribute_values_i < attribute_values.length; attribute_values_i++) {
+
+                if (
+                  attribute_values[attribute_values_i].indexOf('font-size') != -1 ||
+                  attribute_values[attribute_values_i].indexOf('font-family') != -1 ||
+                  attribute_values[attribute_values_i].indexOf('font-weight') != -1 ||
+                  attribute_values[attribute_values_i].indexOf('font-style') != -1
+                ) {
+                  // Not a value we want, remove.
+                  delete attribute_values[attribute_values_i];
+                }
+              }
+
+              if (attribute_values.join() == "") {
+                tag.removeAttribute("style");
+              }
+              else {
+                tag.setAttribute("style", attribute_values.join(''));
+              }
+
+            }
+          }
+
+          event.data.dataValue = div.innerHTML;
+        });
+
+        editor.on('selectionChange', function(event) {
           // Verify the selected content is a link or not. In case it's a link
           // replace the text the user selected with a jQuery selector.
           if (jQuery(this.getSelectedHtml().$).is('a')) {
