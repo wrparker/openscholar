0627166ee269ad81417e46e9b3fe23d9814e93e3
RoySegall <roy.segall@gizra.com>
2017-05-24 13:13:41 +0300

    Start moving docker logic to the docker file.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,66 @@ FROM gizra/drupal-lamp
 ADD . /var/www/html/openscholar
 WORKDIR /var/www/html/openscholar
 
+# Increasing the memory limitation.
+RUN mv docker_files/php.ini /usr/local/etc/php
+
+# Start MySQL.
+RUN echo -e "\n # Start MySQL."
+RUN service mysql start
+
+# Configure apache2.
+RUN echo -e "\n # Configure apache2."
+RUN cp docker_files/default.apache2.conf /etc/apache2/apache2.conf
+RUN service apache2 restart
+
+# Install Bower.
+RUN echo -e "\n # Install Bower."
+RUN npm install -g bower
+RUN echo '{ "allow_root": true }' > /root/.bowerrc
+
+# Install drush.
+RUN echo -e "\n # Install drush."
+RUN export PATH="$HOME/.composer/vendor/bin:$PATH"
+RUN composer global require drush/drush:7.*
+
+RUN mkdir ~/.drush/
+RUN cp docker_files/aliases.drushrc.php ~/.drush/aliases.drushrc.php
+
+# Download and install Apache solr.
+RUN echo -e "\n # Install apache solr."
+RUN wget https://archive.apache.org/dist/lucene/solr/3.6.2/apache-solr-3.6.2.zip
+RUN unzip -oq apache-solr-3.6.2.zip
+RUN cd apache-solr-3.6.2/example/solr/conf
+
+# Copy the solr config files from the apache solr module
+RUN yes | cp /var/www/html/openscholar/www/profiles/openscholar/modules/contrib/apachesolr/solr-conf/solr-3.x/* .
+
+# Copy the modified solrconfig.xml file to commit records every 20 seconds so items will show up in search sooner
+RUN yes | cp /var/www/html/openscholar/www/profiles/openscholar/behat/solr/solrconfig.xml .
+RUN cd ../../
+RUN java -jar start.jar &
+RUN sleep 10
+
+# Install Firefox (iceweasel).
+RUN echo -e "\n${BGCYAN}[RUN] Install firefox. ${RESTORE}"
+RUN apt-get update
+RUN apt-get -qq -y install iceweasel > /dev/null
+
+# Install headless GUI for browser.'Xvfb is a display server that performs
+# graphical operations in memory'.
+RUN echo -e "\n [RUN] Installing XVFB (headless GUI for Firefox).\n"
+RUN apt-get install xvfb -y
+RUN apt-get install openjdk-7-jre-headless -y
+RUN Xvfb :99 -ac &
+RUN export DISPLAY=:99
+RUN sleep 5
+
+# Install Selenium.
+RUN echo -e  "\n${BGCYAN}[RUN] Install Selenium. ${RESTORE}"
+RUN wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar
+RUN java -jar selenium-server-standalone-2.53.0.jar > /dev/null 2>&1 &
+RUN sleep 10
+
 # Add a bash script to finalize all
 RUN chmod +x /var/www/html/openscholar/docker_files/run.sh
 ENTRYPOINT ["/var/www/html/openscholar/docker_files/run.sh"]
--- docker_files/run.sh
+++ docker_files/run.sh
@@ -1,36 +1,8 @@
 #!/usr/bin/env bash
 
-# Increasing the memory limitation.
-mv docker_files/php.ini /usr/local/etc/php
-
-# Start MySQL.
-echo -e "\n # Start MySQL."
-service mysql start
-
-# Configure apache2.
-echo -e "\n # Configure apache2."
-cp docker_files/default.apache2.conf /etc/apache2/apache2.conf
-service apache2 restart
-
-# Install Bower.
-echo -e "\n # Install Bower."
-npm install -g bower
-echo '{ "allow_root": true }' > /root/.bowerrc
-
-# Install drush.
-echo -e "\n # Install drush."
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-composer global require drush/drush:7.*
-
-cd /var/www/html/openscholar
-mkdir ~/.drush/
-cp docker_files/aliases.drushrc.php ~/.drush/aliases.drushrc.php
 source /root/.bashrc
 
-# Install Drupal
-echo -e "\n # Install Drupal"
-bash docker.install.sh
-chmod -R 777 /var/www/html/openscholar/www/sites/default/files/
+cd /var/www/html/openscholar
 
 # Install custom domains
 echo -e "\n # Add lincoln virtual domain."
@@ -40,20 +12,11 @@ sh -c "cat openscholar/behat/lincoln-vhost.txt > /etc/apache2/sites-available/li
 a2ensite lincoln.local
 service apache2 reload
 
-# Download and install Apache solr.
-echo -e "\n # Install apache solr."
-wget https://archive.apache.org/dist/lucene/solr/3.6.2/apache-solr-3.6.2.zip
-unzip -oq apache-solr-3.6.2.zip
-cd apache-solr-3.6.2/example/solr/conf
+# Install Drupal
+echo -e "\n # Install Drupal"
+bash docker.install.sh
+chmod -R 777 /var/www/html/openscholar/www/sites/default/files/
 
-# Copy the solr config files from the apache solr module
-yes | cp /var/www/html/openscholar/www/profiles/openscholar/modules/contrib/apachesolr/solr-conf/solr-3.x/* .
-
-# Copy the modified solrconfig.xml file to commit records every 20 seconds so items will show up in search sooner
-yes | cp /var/www/html/openscholar/www/profiles/openscholar/behat/solr/solrconfig.xml .
-cd ../../
-java -jar start.jar &
-sleep 10
 cd /var/www/html/openscholar
 
 # Index site using Apache solr
@@ -64,26 +27,6 @@ drush solr-mark-all
 drush solr-index
 drush vset oembedembedly_api_key $EMBEDLYAPIKEY
 cd -
-
-# Install Firefox (iceweasel).
-echo -e "\n${BGCYAN}[RUN] Install firefox. ${RESTORE}"
-apt-get update
-apt-get -qq -y install iceweasel > /dev/null
-
-# Install headless GUI for browser.'Xvfb is a display server that performs
-# graphical operations in memory'.
-echo -e "\n [RUN] Installing XVFB (headless GUI for Firefox).\n"
-apt-get install xvfb -y
-apt-get install openjdk-7-jre-headless -y
-Xvfb :99 -ac &
-export DISPLAY=:99
-sleep 5
-
-# Install Selenium.
-echo -e  "\n${BGCYAN}[RUN] Install Selenium. ${RESTORE}"
-wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar
-java -jar selenium-server-standalone-2.53.0.jar > /dev/null 2>&1 &
-sleep 10
 
 # Install behat
 echo -e "\n # Install behat"
@@ -97,8 +40,8 @@ cp behat.local.yml.travis behat.local.yml
 if [ $DOCKER_DEBUG -eq 1 ]; then
   bash
 else
-  # Run tests
-  echo -e "\n # Run tests"
+  # tests
+  echo -e "\n # tests"
   ./bin/behat --tags=$TEST_SUITE
 
 
