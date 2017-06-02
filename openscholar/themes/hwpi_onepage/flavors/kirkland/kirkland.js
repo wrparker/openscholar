5811a2dc8b4607de2efa1bf051d7ef5a453213db
Dan Cabral <rdancabral@gmail.com>
2017-06-02 14:20:42 -0400

    kirkland flavor updates. #9536

--- openscholar/themes/hwpi_onepage/flavors/kirkland/kirkland.js
+++ openscholar/themes/hwpi_onepage/flavors/kirkland/kirkland.js
@@ -1,17 +1,3 @@
- WebFontConfig = {
-     google: {
-         families: ['Oswald:400,300,700:latin', 'Quattrocento']
-     }
- };
- (function() {
-     var wf = document.createElement('script');
-     wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-     wf.type = 'text/javascript';
-     wf.async = 'true';
-     var s = document.getElementsByTagName('script')[0];
-     s.parentNode.insertBefore(wf, s);
- })();
-
  jQuery(document).ready(function() {
 // ADDS A RANDOM CLASS TO THE BODY - APPLY BG IMAGES
      var classes = ["bg-one", "bg-two", "bg-three", "bg-four", "bg-five", "bg-six", "bg-seven", "bg-eight"];
