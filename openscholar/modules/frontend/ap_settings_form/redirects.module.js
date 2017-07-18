c2bbd1408aae641b92550930e13e0ebeb39e56fe
Arijit Banerjee <arijit.banerjee2@cognizant.com>
2017-07-18 12:21:16 +0530

    #9589 Modify description on the add redirect form.

--- openscholar/modules/frontend/ap_settings_form/redirects.module.js
+++ openscholar/modules/frontend/ap_settings_form/redirects.module.js
@@ -83,7 +83,7 @@
       '</ul>' +
       '<a class="redirect-add" ng-show="showAddLink()" ng-click="toggleAddForm()">+ Add new redirect</a>' +
       '<div class="redirect-add-form" ng-show="showAddForm">' +
-        '<div class="display-inline"><label for="redirect-path">Redirect From</label> {{siteBaseUrl}}/<input type="text" id="redirect-path" class="redirect-new-element" ng-model="newRedirectPath" placeholder="Local path"><span class="description">(Example: my-path). Fragment anchors (e.g. #anchor) are not allowed.</span></div>' +
+        '<div class="display-inline"><label for="redirect-path">Redirect From</label> {{siteBaseUrl}}/<input type="text" id="redirect-path" class="redirect-new-element" ng-model="newRedirectPath" placeholder="Local path"><span class="description">(Example: my-path). Fragment anchors (e.g. #anchor) Leading slash( / ) are not allowed.</span></div>' +
         '<div class="display-inline"><label for="redirect-target">Redirect To</label> <input type="text" id="redirect-target" class="redirect-new-element" ng-model="newRedirectTarget" placeholder="Target URL (i.e. http://www.google.com)"><span class="description">Enter any existing destination URL (like http://example.com) to redirect to.</span></div>' +
         '<button type="button" value="Add Redirect" ng-click="addRedirect()"">Add Redirect</button>' +
       '</div>',
@@ -132,7 +132,7 @@
             if (r.data.data.msg != '') {
               var msg = r.data.data.msg;
               scope.errors.push($sce.trustAsHtml(msg));
-            } 
+            }
           });
         }
 
