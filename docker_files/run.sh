772bb36d866fcf35f065db082a31103ed7e8bbdd
Ethan Mallove <mallove@fas.harvard.edu>
2017-07-18 10:25:14 -0400

    run full test suite

--- docker_files/run.sh
+++ docker_files/run.sh
@@ -99,8 +99,7 @@ if [ $DOCKER_DEBUG -eq 1 ]; then
 else
   # Run tests
   echo -e "\n # Run tests"
-  ./bin/behat --verbose --tags="@os_blog"
-
+  ./bin/behat --verbose --tags=$TEST_SUITE
 
   if [ $? -ne 0 ]; then
     echo "Behat failed"
